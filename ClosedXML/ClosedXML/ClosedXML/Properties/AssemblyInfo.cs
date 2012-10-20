348615159777aeb888f944a2bc4f9ac0799362fa
MDeLeon_cp <SND\MDeLeon_cp>
2012-10-19 22:47:44 -0700

    Ready for release

--- ClosedXML/ClosedXML/ClosedXML/Properties/AssemblyInfo.cs
+++ ClosedXML/ClosedXML/ClosedXML/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.68.0.1")]
-[assembly: AssemblyFileVersion("0.68.0.1")]
+[assembly: AssemblyVersion("0.68.1.0")]
+[assembly: AssemblyFileVersion("0.68.1.0")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("ClosedXML_Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a1fb8ba59167fe734d64128ca73d32c45cb8a117246d09c95c8769db88fe332b0a3396bedd0ea48ee42b0e5796fec0798ca5cb628a9a6de80d35d6c67b936ca1670347b3d4f2b769c8ce2ddcf959dbac6bcd88e6c08751ea1fffa0522de3507193e7035305a8aa008d6c88cca1341b3120fa9c347ab3f97e2d772e2709277da5")]
