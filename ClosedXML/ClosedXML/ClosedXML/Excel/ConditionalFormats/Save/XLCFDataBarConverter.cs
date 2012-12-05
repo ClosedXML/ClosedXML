c5aaa628c8550ed1deec66958fdd57ec315fba6d
MDeLeon_cp <SND\MDeLeon_cp>
2012-12-05 09:13:06 -0800

    Fixed issue loading conditional formatting data bars that use min/max values

--- ClosedXML/ClosedXML/ClosedXML/Excel/ConditionalFormats/Save/XLCFDataBarConverter.cs
+++ ClosedXML/ClosedXML/ClosedXML/Excel/ConditionalFormats/Save/XLCFDataBarConverter.cs
@@ -14,8 +14,12 @@ namespace ClosedXML.Excel
             var conditionalFormattingRule = new ConditionalFormattingRule { Type = cf.ConditionalFormatType.ToOpenXml(), Priority = priority };
 
             var dataBar = new DataBar {ShowValue = !cf.ShowBarOnly};
-            var conditionalFormatValueObject1 = new ConditionalFormatValueObject { Type = cf.ContentTypes[1].ToOpenXml() , Val = cf.Values[1].Value };
-            var conditionalFormatValueObject2 = new ConditionalFormatValueObject { Type = cf.ContentTypes[2].ToOpenXml(), Val = cf.Values[2].Value };
+            var conditionalFormatValueObject1 = new ConditionalFormatValueObject { Type = cf.ContentTypes[1].ToOpenXml()};
+            if (cf.Values.Count >= 1) conditionalFormatValueObject1.Val = cf.Values[1].Value;
+
+            var conditionalFormatValueObject2 = new ConditionalFormatValueObject { Type = cf.ContentTypes[2].ToOpenXml()};
+            if (cf.Values.Count >= 2) conditionalFormatValueObject2.Val = cf.Values[2].Value;
+
             var color = new Color { Rgb = cf.Colors[1].Color.ToHex() };
 
             dataBar.Append(conditionalFormatValueObject1);
