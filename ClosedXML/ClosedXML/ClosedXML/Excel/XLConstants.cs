5a7fd3820f5634ecbd0f5d7c4a868c0255c4a046
NickNack2020 <NickNack2020@gmail.com>
2014-07-26 00:53:28 -0400

    Adding constants file

---
+++ ClosedXML/ClosedXML/ClosedXML/Excel/XLConstants.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ClosedXML.Excel
+{
+    //Use the class to store magic strings or variables.
+    internal static class XLConstants
+    {
+        internal static class Comment
+        {
+            internal const string ShapeTypeId = "#_x0000_t202";
+            internal const string AlternateShapeTypeId = "#_xssf_cell_comment";
+        }
+    }
+}
