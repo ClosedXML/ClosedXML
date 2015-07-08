fdc90c609a33888d83cfc43239c9b8c5f8fedea2
unknown <awhiteway@Aaron-PC.mw.local>
2015-07-08 11:02:27 -0300

    Fixed a bug for when the case when no images are added.
    Signed-off-by: Aaron Whiteway <awhiteway@Aaron-PC.mw.local>

--- ClosedXML/Excel/XLWorkbook_Save.cs
+++ ClosedXML/Excel/XLWorkbook_Save.cs
@@ -4698,7 +4698,7 @@ namespace ClosedXML.Excel
                 }
             }
 
-            if (xlWorksheet.Pictures().Count > 0)
+            if (xlWorksheet.Pictures() != null && xlWorksheet.Pictures().Count > 0)
             {
                 Drawing worksheetDrawing = new Drawing { Id = worksheetPart.GetIdOfPart(worksheetPart.DrawingsPart) };
                 worksheetDrawing.AddNamespaceDeclaration("r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
