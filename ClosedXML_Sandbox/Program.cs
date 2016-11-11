6b84c9e63d37e28c3d575f7be8a78dbc7842a827
Francois Botha <igitur@gmail.com>
2016-11-11 11:29:27 +0200

    Revert accidentally committed files.

--- ClosedXML_Sandbox/Program.cs
+++ ClosedXML_Sandbox/Program.cs
@@ -1,6 +1,4 @@
-﻿using ClosedXML.Excel;
-using System.Linq;
-using System;
+﻿using System;
 
 namespace ClosedXML_Sandbox
 {
@@ -8,23 +6,7 @@ namespace ClosedXML_Sandbox
     {
         private static void Main(string[] args)
         {
-            var path = "tmp.xlsx";
-            using (var workbook = new XLWorkbook(path))
-            {
-                var ws1 = workbook.Worksheets.Last();
-
-                int rowCount = 20; //example
-                                   //first row headers
-                for (int i = 1; i < rowCount; i++)
-                {
-                    var row = ws1.Row(i);
-                    var values = row.Cells().Select(c => c.Value).ToArray();
-                    IXLCell cell = row.Cell("E");     //.Cell(5);
-                    bool isEmpty = cell.IsEmpty();      //always empty on this column, but in original have 6 lines
-                    var val = cell.Value;   //cell.
-                                            //vals += val + Environment.NewLine;
-                }
-            }
+            PerformanceRunner.TimeAction(PerformanceRunner.RunInsertTable);
 
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
