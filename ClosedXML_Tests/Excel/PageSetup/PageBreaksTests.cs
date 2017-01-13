d458a4fb5d49b867d87df274baeaf2d436abfc0a
Rozhkov Alexey <b0bi@yandex.ru>
2017-01-13 09:51:18 +0300

    Shift page breaks

---
+++ ClosedXML_Tests/Excel/PageSetup/PageBreaksTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using ClosedXML.Excel;
+using NUnit.Framework;
+
+namespace ClosedXML_Tests.Excel
+{
+    [TestFixture]
+    public class PageBreaksTests
+    {
+        [Test]
+        public void RowBreaksShiftWhenInsertedRowAbove()
+        {
+            var wb = new XLWorkbook();
+            IXLWorksheet sheet = wb.AddWorksheet("Sheet1");
+
+            sheet.PageSetup.AddHorizontalPageBreak(10); 
+            sheet.Row(5).InsertRowsAbove(1);
+            Assert.AreEqual(11, sheet.PageSetup.RowBreaks[0]);
+        }        
+
+        [Test]
+        public void RowBreaksNotShiftWhenInsertedRowBelow()
+        {
+            var wb = new XLWorkbook();
+            IXLWorksheet sheet = wb.AddWorksheet("Sheet1");
+
+            sheet.PageSetup.AddHorizontalPageBreak(10); 
+            sheet.Row(15).InsertRowsAbove(1);
+            Assert.AreEqual(10, sheet.PageSetup.RowBreaks[0]);
+        }        
+
+        [Test]
+        public void RowBreaksShiftWhenInsertedColumn1()
+        {
+            var wb = new XLWorkbook();
+            IXLWorksheet sheet = wb.AddWorksheet("Sheet1");
+
+            sheet.PageSetup.AddVerticalPageBreak(10); 
+            sheet.Column(5).InsertColumnsBefore(1);
+            Assert.AreEqual(11, sheet.PageSetup.ColumnBreaks[0]);
+        }        
+
+        [Test]
+        public void RowBreaksNotShiftWhenInsertedColumn()
+        {
+            var wb = new XLWorkbook();
+            IXLWorksheet sheet = wb.AddWorksheet("Sheet1");
+
+            sheet.PageSetup.AddVerticalPageBreak(10);
+            sheet.Column(15).InsertColumnsBefore(1);
+            Assert.AreEqual(10, sheet.PageSetup.ColumnBreaks[0]);
+        }        
+    }
+}
