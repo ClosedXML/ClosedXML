44757eee24c7d5cfb1f93102c6a0190c8c6c1f80
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2017-03-04 22:17:58 +0100

    Keep the ColumnOrderAttribute as Obsolete to help developers migrate to the XLColumnAttribute

---
+++ ClosedXML/Attributes/ColumnOrderAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClosedXML.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    [Obsolete("Use XLColumnAttribute instead")]
+    public class ColumnOrderAttribute : Attribute
+    {
+        // Deprecated, use XLColumnAttribute instead
+
+        // This attribute should be kept for "a while" to allow existing users to find the new XLColumnAttribute
+        // Should be removed before ClosedXML turns version 1.0
+    }
+}
