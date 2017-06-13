c90ce9240fed284a7d61bd3fa03468dd4ba79a36
Rozhkov Alexey <b0bi@yandex.ru>
2017-06-13 15:32:38 +0300

    Added tests for RangeBase.Relative and RangeBase.Crop

--- ClosedXML_Tests/Excel/Ranges/XLRangeBaseTests.cs
+++ ClosedXML_Tests/Excel/Ranges/XLRangeBaseTests.cs
@@ -275,5 +275,32 @@ namespace ClosedXML_Tests
 
 
         //}
+
+        [Test]
+        public void RelativeTest()
+        {
+            var wb = new XLWorkbook();
+            IXLWorksheet ws = wb.Worksheets.Add("Sheet");
+            var range = ws.Range("C11:D12");
+            var baseRange = ws.Range("B10:E13");
+            var targetRange = ws.Range("C3:F6");
+
+            var assert = range.Relative(baseRange, targetRange);
+            Assert.AreEqual("D4", assert.RangeAddress.FirstAddress.ToStringRelative());
+            Assert.AreEqual("E5", assert.RangeAddress.LastAddress.ToStringRelative());
+        }
+
+        [Test]
+        public void CropTest()
+        {
+            var wb = new XLWorkbook();
+            IXLWorksheet ws = wb.Worksheets.Add("Sheet");
+            var range = ws.Range("B3:D8");
+            var crop = ws.Range("A5:E6");
+            var assert = range.Crop(crop);
+
+            Assert.AreEqual("B5", assert.RangeAddress.FirstAddress.ToStringRelative());
+            Assert.AreEqual("D6", assert.RangeAddress.LastAddress.ToStringRelative());
+        }
     }
 }
