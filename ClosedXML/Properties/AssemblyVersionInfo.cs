8bcb3b7b35dc70ac91b110f66663a648221d18bc
Francois Botha <igitur@gmail.com>
2017-06-22 14:17:37 +0200

    Consolidate assembly version info into one file

---
+++ ClosedXML/Properties/AssemblyVersionInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.80.0.0")]
+[assembly: AssemblyFileVersion("0.80.0.0")]
