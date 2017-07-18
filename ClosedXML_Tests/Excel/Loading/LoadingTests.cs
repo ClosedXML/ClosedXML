8b68a44bcc0c942595f2df7bcf0a6f77b3f5e9ef
Francois Botha <igitur@gmail.com>
2017-07-18 17:56:48 +0200

    Fall back to display name if table name is missing.

--- ClosedXML_Tests/Excel/Loading/LoadingTests.cs
+++ ClosedXML_Tests/Excel/Loading/LoadingTests.cs
@@ -27,7 +27,8 @@ namespace ClosedXML_Tests.Excel
                 @"Misc\InvalidPrintTitles.xlsx",
                 @"Misc\ExcelProducedWorkbookWithImages.xlsx",
                 @"Misc\EmptyCellValue.xlsx",
-                @"Misc\AllShapes.xlsx"
+                @"Misc\AllShapes.xlsx",
+                @"Misc\TableWithNameNull.xlsx"
             };
 
             foreach (var file in files)
