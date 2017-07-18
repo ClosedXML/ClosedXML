f10ff2c41aabc8e98e75b9b861965368a87eb12d
Francois Botha <igitur@gmail.com>
2017-07-18 11:38:23 +0200

    Fully implement FLOOR function

---
+++ ClosedXML_Tests/Excel/CalcEngine/MathTrigTests.cs
@@ -0,0 +1,65 @@
+﻿using ClosedXML.Excel;
+using NUnit.Framework;
+using System;
+
+namespace ClosedXML_Tests.Excel.CalcEngine
+{
+    [TestFixture]
+    public class MathTrigTests
+    {
+        [Test]
+        public void Floor()
+        {
+            Object actual;
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(1.2)");
+            Assert.AreEqual(1, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(1.7)");
+            Assert.AreEqual(1, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(-1.7)");
+            Assert.AreEqual(-2, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(1.2, 1)");
+            Assert.AreEqual(1, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(1.7, 1)");
+            Assert.AreEqual(1, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(-1.7, 1)");
+            Assert.AreEqual(-2, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(0.4, 2)");
+            Assert.AreEqual(0, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(2.7, 2)");
+            Assert.AreEqual(2, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(7.8, 2)");
+            Assert.AreEqual(6, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR(-5.5, -2)");
+            Assert.AreEqual(-4, actual);
+        }
+
+        //[Test]
+        // Functions have to support a period first before we can implement this
+        public void FloorMath()
+        {
+            Object actual;
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR.MATH(24.3, 5)");
+            Assert.AreEqual(20, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR.MATH(6.7)");
+            Assert.AreEqual(6, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR.MATH(-8.1, 2)");
+            Assert.AreEqual(-10, actual);
+
+            actual = XLWorkbook.EvaluateExpr(@"FLOOR.MATH(-5.5, 2, -1)");
+            Assert.AreEqual(-4, actual);
+        }
+    }
+}
