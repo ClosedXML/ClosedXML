668dca80306fcf3d781d7cf8eb69cb029851586d
Francois Botha <igitur@gmail.com>
2017-11-02 13:55:02 +0200

    Sorted methods/fields alphabetically according to CodeMaid conventions

--- ClosedXML/Excel/IXLWorkbook.cs
+++ ClosedXML/Excel/IXLWorkbook.cs
@@ -8,55 +8,7 @@ namespace ClosedXML.Excel
 {
     public interface IXLWorkbook : IDisposable
     {
-        /// <summary>
-        ///   Gets an object to manipulate the worksheets.
-        /// </summary>
-        IXLWorksheets Worksheets { get; }
-
-        /// <summary>
-        ///   Gets an object to manipulate this workbook's named ranges.
-        /// </summary>
-        IXLNamedRanges NamedRanges { get; }
-
-        /// <summary>
-        ///   Gets an object to manipulate this workbook's theme.
-        /// </summary>
-        IXLTheme Theme { get; }
-
-        /// <summary>
-        ///   Gets or sets the default style for the workbook.
-        ///   <para>All new worksheets will use this style.</para>
-        /// </summary>
-        IXLStyle Style { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the default row height for the workbook.
-        ///   <para>All new worksheets will use this row height.</para>
-        /// </summary>
-        Double RowHeight { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the default column width for the workbook.
-        ///   <para>All new worksheets will use this column width.</para>
-        /// </summary>
-        Double ColumnWidth { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the default page options for the workbook.
-        ///   <para>All new worksheets will use these page options.</para>
-        /// </summary>
-        IXLPageSetup PageOptions { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the default outline options for the workbook.
-        ///   <para>All new worksheets will use these outline options.</para>
-        /// </summary>
-        IXLOutline Outline { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the workbook's properties.
-        /// </summary>
-        XLWorkbookProperties Properties { get; set; }
+        String Author { get; set; }
 
         /// <summary>
         ///   Gets or sets the workbook's calculation mode.
@@ -64,25 +16,16 @@ namespace ClosedXML.Excel
         XLCalculateMode CalculateMode { get; set; }
 
         Boolean CalculationOnSave { get; set; }
-        Boolean ForceFullCalculation { get; set; }
-        Boolean FullCalculationOnLoad { get; set; }
-        Boolean FullPrecision { get; set; }
 
         /// <summary>
-        ///   Gets or sets the workbook's reference style.
+        ///   Gets or sets the default column width for the workbook.
+        ///   <para>All new worksheets will use this column width.</para>
         /// </summary>
-        XLReferenceStyle ReferenceStyle { get; set; }
+        Double ColumnWidth { get; set; }
 
         IXLCustomProperties CustomProperties { get; }
 
-        Boolean ShowFormulas { get; set; }
-        Boolean ShowGridLines { get; set; }
-        Boolean ShowOutlineSymbols { get; set; }
-        Boolean ShowRowColHeaders { get; set; }
-        Boolean ShowRuler { get; set; }
-        Boolean ShowWhiteSpace { get; set; }
-        Boolean ShowZeros { get; set; }
-        Boolean RightToLeft { get; set; }
+        Boolean DefaultRightToLeft { get; }
 
         Boolean DefaultShowFormulas { get; }
 
@@ -98,13 +41,128 @@ namespace ClosedXML.Excel
 
         Boolean DefaultShowZeros { get; }
 
-        Boolean DefaultRightToLeft { get; }
+        Boolean ForceFullCalculation { get; set; }
+
+        Boolean FullCalculationOnLoad { get; set; }
+
+        Boolean FullPrecision { get; set; }
+
+        Boolean IsPasswordProtected { get; }
+
+        Boolean LockStructure { get; set; }
+
+        Boolean LockWindows { get; set; }
+
+        /// <summary>
+        ///   Gets an object to manipulate this workbook's named ranges.
+        /// </summary>
+        IXLNamedRanges NamedRanges { get; }
+
+        /// <summary>
+        ///   Gets or sets the default outline options for the workbook.
+        ///   <para>All new worksheets will use these outline options.</para>
+        /// </summary>
+        IXLOutline Outline { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the default page options for the workbook.
+        ///   <para>All new worksheets will use these page options.</para>
+        /// </summary>
+        IXLPageSetup PageOptions { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the workbook's properties.
+        /// </summary>
+        XLWorkbookProperties Properties { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the workbook's reference style.
+        /// </summary>
+        XLReferenceStyle ReferenceStyle { get; set; }
+
+        Boolean RightToLeft { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the default row height for the workbook.
+        ///   <para>All new worksheets will use this row height.</para>
+        /// </summary>
+        Double RowHeight { get; set; }
+
+        Boolean ShowFormulas { get; set; }
+
+        Boolean ShowGridLines { get; set; }
+
+        Boolean ShowOutlineSymbols { get; set; }
+
+        Boolean ShowRowColHeaders { get; set; }
+
+        Boolean ShowRuler { get; set; }
+
+        Boolean ShowWhiteSpace { get; set; }
+
+        Boolean ShowZeros { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the default style for the workbook.
+        ///   <para>All new worksheets will use this style.</para>
+        /// </summary>
+        IXLStyle Style { get; set; }
+
+        /// <summary>
+        ///   Gets an object to manipulate this workbook's theme.
+        /// </summary>
+        IXLTheme Theme { get; }
+
+        Boolean Use1904DateSystem { get; set; }
+
+        /// <summary>
+        ///   Gets an object to manipulate the worksheets.
+        /// </summary>
+        IXLWorksheets Worksheets { get; }
+
+        IXLWorksheet AddWorksheet(String sheetName);
+
+        IXLWorksheet AddWorksheet(String sheetName, Int32 position);
+
+        IXLWorksheet AddWorksheet(DataTable dataTable);
+
+        void AddWorksheet(DataSet dataSet);
+
+        void AddWorksheet(IXLWorksheet worksheet);
+
+        IXLWorksheet AddWorksheet(DataTable dataTable, String sheetName);
+
+        IXLCell Cell(String namedCell);
+
+        IXLCells Cells(String namedCells);
+
+        IXLCustomProperty CustomProperty(String name);
+
+        Object Evaluate(String expression);
+
+        IXLCells FindCells(Func<IXLCell, Boolean> predicate);
+
+        IXLColumns FindColumns(Func<IXLColumn, Boolean> predicate);
+
+        IXLRows FindRows(Func<IXLRow, Boolean> predicate);
 
         IXLNamedRange NamedRange(String rangeName);
 
-        Boolean TryGetWorksheet(String name, out IXLWorksheet worksheet);
+        void Protect(Boolean lockStructure, Boolean lockWindows, String workbookPassword);
+
+        void Protect();
+
+        void Protect(string workbookPassword);
+
+        void Protect(Boolean lockStructure);
+
+        void Protect(Boolean lockStructure, Boolean lockWindows);
+
+        IXLRange Range(String range);
 
         IXLRange RangeFromFullAddress(String rangeAddress, out IXLWorksheet ws);
+
+        IXLRanges Ranges(String ranges);
 
         /// <summary>
         ///   Saves the current workbook.
@@ -142,18 +200,6 @@ namespace ClosedXML.Excel
 
         void SaveAs(Stream stream, SaveOptions options);
 
-        IXLWorksheet Worksheet(String name);
-
-        IXLWorksheet Worksheet(Int32 position);
-
-        IXLCustomProperty CustomProperty(String name);
-
-        IXLCells FindCells(Func<IXLCell, Boolean> predicate);
-
-        IXLRows FindRows(Func<IXLRow, Boolean> predicate);
-
-        IXLColumns FindColumns(Func<IXLColumn, Boolean> predicate);
-
         /// <summary>
         /// Searches the cells' contents for a given piece of text
         /// </summary>
@@ -163,58 +209,22 @@ namespace ClosedXML.Excel
         /// <returns></returns>
         IEnumerable<IXLCell> Search(String searchText, CompareOptions compareOptions = CompareOptions.Ordinal, Boolean searchFormulae = false);
 
-        IXLCell Cell(String namedCell);
+        XLWorkbook SetLockStructure(Boolean value);
 
-        IXLCells Cells(String namedCells);
-
-        IXLRange Range(String range);
-
-        IXLRanges Ranges(String ranges);
-
-        Boolean Use1904DateSystem { get; set; }
+        XLWorkbook SetLockWindows(Boolean value);
 
         XLWorkbook SetUse1904DateSystem();
 
         XLWorkbook SetUse1904DateSystem(Boolean value);
 
-        IXLWorksheet AddWorksheet(String sheetName);
-
-        IXLWorksheet AddWorksheet(String sheetName, Int32 position);
-
-        IXLWorksheet AddWorksheet(DataTable dataTable);
-
-        void AddWorksheet(DataSet dataSet);
-
-        void AddWorksheet(IXLWorksheet worksheet);
-
-        IXLWorksheet AddWorksheet(DataTable dataTable, String sheetName);
-
-        Object Evaluate(String expression);
-
-        String Author { get; set; }
-
-        Boolean LockStructure { get; set; }
-
-        XLWorkbook SetLockStructure(Boolean value);
-
-        Boolean LockWindows { get; set; }
-
-        XLWorkbook SetLockWindows(Boolean value);
-
-        Boolean IsPasswordProtected { get; }
-
-        void Protect(Boolean lockStructure, Boolean lockWindows, String workbookPassword);
-
-        void Protect();
-
-        void Protect(string workbookPassword);
-
-        void Protect(Boolean lockStructure);
-
-        void Protect(Boolean lockStructure, Boolean lockWindows);
+        Boolean TryGetWorksheet(String name, out IXLWorksheet worksheet);
 
         void Unprotect();
 
         void Unprotect(string workbookPassword);
+
+        IXLWorksheet Worksheet(String name);
+
+        IXLWorksheet Worksheet(Int32 position);
     }
 }
