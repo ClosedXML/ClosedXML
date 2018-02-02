14fa0e22b32b6688dd0a3ff90ae0152ef9d30a11
Mathias Fischler <Mafii@users.noreply.github.com>
2018-02-02 15:20:10 +0100

    Update OpenXmlHelper.cs

--- ClosedXML/Utils/OpenXmlHelper.cs
+++ ClosedXML/Utils/OpenXmlHelper.cs
@@ -11,7 +11,7 @@ namespace ClosedXML.Utils
 
         public static bool GetBooleanValueAsBool(BooleanValue value, bool defaultValue)
         {
-            return value.HasValue ? defaultValue : value.Value;
+            return value.HasValue ? value.Value : defaultValue;
         }
     }
 }
