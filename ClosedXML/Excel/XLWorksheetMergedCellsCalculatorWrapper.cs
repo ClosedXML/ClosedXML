8ba3e2dfc0d4d3566426d8250e2d7c353314e89f
crypto_rsa <pkotrc@gmail.com>
2018-04-20 11:29:31 +0200

    Add class XLWorksheetMergedCellsCalculatorWrapper

---
+++ ClosedXML/Excel/XLWorksheetMergedCellsCalculatorWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClosedXML.Excel
+{
+    /// <summary>
+    /// A wrapper class which calculates the merged cells of a collection of worksheets
+    /// and resets them in the <see cref="Dispose"/> method
+    /// </summary>
+    internal class XLWorksheetMergedCellsCalculatorWrapper : IDisposable
+    {
+        private readonly List<XLWorksheet> _worksheets;
+
+        public XLWorksheetMergedCellsCalculatorWrapper(IEnumerable<XLColumn> columns)
+        {
+            _worksheets = columns.Select(c => c.Worksheet).Distinct().ToList();
+            _worksheets.ForEach(w => w.Internals.MergedRanges.CalculateMergedCells());
+        }
+
+        public void Dispose()
+        {
+            _worksheets.ForEach(w => w.Internals.MergedRanges.ResetMergedCells());
+        }
+    }
+}
