d8fd48727d62d376a4db1092e6f4b0a05cfa524a
Francois Botha <igitur@gmail.com>
2018-04-24 09:58:04 +0200

    CodeMaid reorganise and clean XLPivotTables

--- ClosedXML/Excel/PivotTables/XLPivotTables.cs
+++ ClosedXML/Excel/PivotTables/XLPivotTables.cs
@@ -1,13 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace ClosedXML.Excel
 {
-    internal class XLPivotTables: IXLPivotTables
+    internal class XLPivotTables : IXLPivotTables
     {
         private readonly Dictionary<String, XLPivotTable> _pivotTables = new Dictionary<string, XLPivotTable>();
+
+        public void Add(String name, IXLPivotTable pivotTable)
+        {
+            _pivotTables.Add(name, (XLPivotTable)pivotTable);
+        }
+
+        public IXLPivotTable AddNew(string name, IXLCell target, IXLRange source)
+        {
+            var pivotTable = new XLPivotTable { Name = name, TargetCell = target, SourceRange = source };
+            _pivotTables.Add(name, pivotTable);
+            return pivotTable;
+        }
+
+        public void Delete(String name)
+        {
+            _pivotTables.Remove(name);
+        }
+
+        public void DeleteAll()
+        {
+            _pivotTables.Clear();
+        }
+
         public IEnumerator<IXLPivotTable> GetEnumerator()
         {
             return _pivotTables.Values.Cast<IXLPivotTable>().GetEnumerator();
@@ -22,30 +44,10 @@ namespace ClosedXML.Excel
         {
             return _pivotTables[name];
         }
+
         IXLPivotTable IXLPivotTables.PivotTable(String name)
         {
             return PivotTable(name);
         }
-        public void Delete(String name)
-        {
-            _pivotTables.Remove(name);
-        }
-        public void DeleteAll()
-        {
-            _pivotTables.Clear();
-        }
-
-        public void Add(String name, IXLPivotTable pivotTable)
-        {
-            _pivotTables.Add(name, (XLPivotTable)pivotTable);
-        }
-
-        public IXLPivotTable AddNew(string name, IXLCell target, IXLRange source)
-        {
-            var pivotTable = new XLPivotTable { Name = name, TargetCell = target, SourceRange = source };
-            _pivotTables.Add(name, pivotTable);
-            return pivotTable;
-        }
-
     }
 }
