2f8820fcd88cbd60f544e36177c9e980b5eb2696
Francois Botha <igitur@gmail.com>
2018-05-28 12:05:40 +0200

    Correctly unprotect worksheet without password

---
+++ ClosedXML_Tests/Excel/Worksheets/XLSheetProtectionTests.cs
@@ -0,0 +1,40 @@
+﻿using ClosedXML.Excel;
+using NUnit.Framework;
+using System;
+
+namespace ClosedXML_Tests.Excel.Worksheets
+{
+    [TestFixture]
+    public class XLSheetProtectionTests
+    {
+        [Test]
+        public void TestUnprotectWorksheetWithNoPassword()
+        {
+            using (var stream = TestHelper.GetStreamFromResource(TestHelper.GetResourcePath(@"Misc\SHA512PasswordProtection.xlsx")))
+            using (var wb = new XLWorkbook(stream))
+            {
+                var ws = wb.Worksheet("Sheet1");
+                Assert.IsTrue(ws.Protection.Protected);
+                ws.Unprotect();
+                Assert.IsFalse(ws.Protection.Protected);
+            }
+        }
+
+        [Test]
+        public void TestWorksheetWithSHA512Protection()
+        {
+            using (var stream = TestHelper.GetStreamFromResource(TestHelper.GetResourcePath(@"Misc\SHA512PasswordProtection.xlsx")))
+            using (var wb = new XLWorkbook(stream))
+            {
+                var ws = wb.Worksheet("Sheet2");
+                Assert.IsTrue(ws.Protection.Protected);
+                // Protected with SHA-512 password - not yet supported.
+                Assert.Throws<ArgumentException>(() => ws.Unprotect());
+
+                // Protected with SHA-512 password - not yet supported.
+                Assert.Throws<ArgumentException>(() => ws.Unprotect("abc"));
+                Assert.IsTrue(ws.Protection.Protected);
+            }
+        }
+    }
+}
