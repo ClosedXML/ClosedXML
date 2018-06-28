bf5358035e6e487890ef8cd350a707aeba608eae
Francois Botha <igitur@gmail.com>
2018-06-28 14:26:36 +0200

    Improve doctext to indicate relativity of Row() and Rows() parameters

--- ClosedXML/Excel/Tables/IXLTableRange.cs
+++ ClosedXML/Excel/Tables/IXLTableRange.cs
@@ -23,10 +23,21 @@ namespace ClosedXML.Excel
 
         IXLTableRow LastRowUsed(Func<IXLTableRow, Boolean> predicate = null);
 
+        /// <summary>
+        /// Rows the specified row.
+        /// </summary>
+        /// <param name="row">1-based row number relative to the first row of this range.</param>
+        /// <returns></returns>
         new IXLTableRow Row(int row);
 
         IXLTableRows Rows(Func<IXLTableRow, Boolean> predicate = null);
 
+        /// <summary>
+        /// Returns a subset of the rows
+        /// </summary>
+        /// <param name="firstRow">The first row to return. 1-based row number relative to the first row of this range.</param>
+        /// <param name="lastRow">The last row to return. 1-based row number relative to the first row of this range.</param>
+        /// <returns></returns>
         new IXLTableRows Rows(int firstRow, int lastRow);
 
         new IXLTableRows Rows(string rows);
