9a66361a922361d5246326d5f3c21dd3587480f9
Rozhkov Alexey <b0bi@yandex.ru>
2018-09-06 20:24:10 +0300

    FIX for: XLConditionalFormat equality comparer fails for ColorScale #987

--- ClosedXML/Excel/ConditionalFormats/XLConditionalFormat.cs
+++ ClosedXML/Excel/ConditionalFormats/XLConditionalFormat.cs
@@ -28,10 +28,10 @@ namespace ClosedXML.Excel
                 if (ReferenceEquals(yy, null)) return false;
                 if (xx.GetType() != yy.GetType()) return false;
 
-                var xxValues = xx.Values.Values.Where(v => !v.IsFormula).Select(v => v.Value);
-                var yyValues = yy.Values.Values.Where(v => !v.IsFormula).Select(v => v.Value);
-                var xxFormulas = x.Ranges.Any() ? xx.Values.Values.Where(v => v.IsFormula).Select(f => ((XLCell)x.Ranges.First().FirstCell()).GetFormulaR1C1(f.Value)) : null;
-                var yyFormulas = y.Ranges.Any() ? yy.Values.Values.Where(v => v.IsFormula).Select(f => ((XLCell)y.Ranges.First().FirstCell()).GetFormulaR1C1(f.Value)) : null;
+                var xxValues = xx.Values.Values.Where(v => v == null || !v.IsFormula).Select(v => v?.Value);
+                var yyValues = yy.Values.Values.Where(v => v == null || !v.IsFormula).Select(v => v?.Value);
+                var xxFormulas = x.Ranges.Any() ? xx.Values.Values.Where(v => v != null && v.IsFormula).Select(f => ((XLCell)x.Ranges.First().FirstCell()).GetFormulaR1C1(f.Value)) : null;
+                var yyFormulas = y.Ranges.Any() ? yy.Values.Values.Where(v => v != null && v.IsFormula).Select(f => ((XLCell)y.Ranges.First().FirstCell()).GetFormulaR1C1(f.Value)) : null;
 
                 var xStyle = xx.StyleValue;
                 var yStyle = yy.StyleValue;
