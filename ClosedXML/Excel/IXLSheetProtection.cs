f9523bfaa14f02f5ec8d80057648c0655759e1f3
Francois Botha <igitur@gmail.com>
2018-10-19 14:09:13 +0200

    CodeMaid organise and clean IXLSheetProtection.cs

--- ClosedXML/Excel/IXLSheetProtection.cs
+++ ClosedXML/Excel/IXLSheetProtection.cs
@@ -1,11 +1,10 @@
+// Keep this file CodeMaid organised and cleaned
 using System;
 
 namespace ClosedXML.Excel
 {
     public interface IXLSheetProtection
     {
-        Boolean Protected { get; set; }
-
         Boolean AutoFilter { get; set; }
         Boolean DeleteColumns { get; set; }
         Boolean DeleteRows { get; set; }
@@ -17,30 +16,48 @@ namespace ClosedXML.Excel
         Boolean InsertRows { get; set; }
         Boolean Objects { get; set; }
         Boolean PivotTables { get; set; }
+        Boolean Protected { get; set; }
         Boolean Scenarios { get; set; }
         Boolean SelectLockedCells { get; set; }
         Boolean SelectUnlockedCells { get; set; }
         Boolean Sort { get; set; }
 
+        IXLSheetProtection Protect();
+
+        IXLSheetProtection Protect(String password);
+
         IXLSheetProtection SetAutoFilter(); IXLSheetProtection SetAutoFilter(Boolean value);
+
         IXLSheetProtection SetDeleteColumns(); IXLSheetProtection SetDeleteColumns(Boolean value);
+
         IXLSheetProtection SetDeleteRows(); IXLSheetProtection SetDeleteRows(Boolean value);
+
         IXLSheetProtection SetFormatCells(); IXLSheetProtection SetFormatCells(Boolean value);
+
         IXLSheetProtection SetFormatColumns(); IXLSheetProtection SetFormatColumns(Boolean value);
+
         IXLSheetProtection SetFormatRows(); IXLSheetProtection SetFormatRows(Boolean value);
+
         IXLSheetProtection SetInsertColumns(); IXLSheetProtection SetInsertColumns(Boolean value);
+
         IXLSheetProtection SetInsertHyperlinks(); IXLSheetProtection SetInsertHyperlinks(Boolean value);
+
         IXLSheetProtection SetInsertRows(); IXLSheetProtection SetInsertRows(Boolean value);
+
         IXLSheetProtection SetObjects(); IXLSheetProtection SetObjects(Boolean value);
+
         IXLSheetProtection SetPivotTables(); IXLSheetProtection SetPivotTables(Boolean value);
+
         IXLSheetProtection SetScenarios(); IXLSheetProtection SetScenarios(Boolean value);
+
         IXLSheetProtection SetSelectLockedCells(); IXLSheetProtection SetSelectLockedCells(Boolean value);
+
         IXLSheetProtection SetSelectUnlockedCells(); IXLSheetProtection SetSelectUnlockedCells(Boolean value);
+
         IXLSheetProtection SetSort(); IXLSheetProtection SetSort(Boolean value);
 
-        IXLSheetProtection Protect();
-        IXLSheetProtection Protect(String password);
         IXLSheetProtection Unprotect();
+
         IXLSheetProtection Unprotect(String password);
     }
 }
