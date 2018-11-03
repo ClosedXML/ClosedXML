23784ff206ccd5f93cd3e79cccd48901a3933c4f
Francois Botha <igitur@gmail.com>
2018-11-03 14:32:19 +0200

    Add LoadOptions parameter to XLWorkbook constructor to optionally recalculate all formulas on loading

---
+++ ClosedXML/Excel/LoadOptions.cs
@@ -0,0 +1,11 @@
+﻿// Keep this file CodeMaid organised and cleaned
+using System;
+
+namespace ClosedXML.Excel
+{
+    public class LoadOptions
+    {
+        public XLEventTracking EventTracking { get; set; } = XLEventTracking.Enabled;
+        public Boolean RecalculateAllFormulas { get; set; } = false;
+    }
+}
