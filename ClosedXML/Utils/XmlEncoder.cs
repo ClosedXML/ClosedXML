a89e3de6e6f6b49e7434ee74f4146803f884d15d
Francois Botha <igitur@gmail.com>
2019-03-06 11:00:48 +0200

    Remove own implementation of `IsXmlChar`

--- ClosedXML/Utils/XmlEncoder.cs
+++ ClosedXML/Utils/XmlEncoder.cs
@@ -8,20 +8,6 @@ namespace ClosedXML.Utils
     {
         private static readonly Regex xHHHHRegex = new Regex("_(x[\\dA-F]{4})_", RegexOptions.Compiled);
 
-        /// <summary>
-        /// Checks if a character is not allowed to the XML Spec http://www.w3.org/TR/REC-xml/#charsets
-        /// </summary>
-        /// <param name="ch">Input Character</param>
-        /// <returns>Returns false if the character is invalid according to the XML specification, and will not be
-        /// escaped by an XmlWriter.</returns>
-        public static bool IsXmlChar(char ch)
-        {
-            return (((ch >= 0x0020 && ch <= 0xD7FF) ||
-                      (ch >= 0xE000 && ch <= 0xFFFD) ||
-                      ch == 0x0009 || ch == 0x000A ||
-                      ch == 0x000D));
-        }
-
         public static string EncodeString(string encodeStr)
         {
             if (encodeStr == null) return null;
@@ -32,7 +18,7 @@ namespace ClosedXML.Utils
 
             foreach (var ch in encodeStr)
             {
-                if (IsXmlChar(ch)) //this method is new in .NET 4
+                if (XmlConvert.IsXmlChar(ch))
                 {
                     sb.Append(ch);
                 }
