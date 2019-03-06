4b33973e3f7a2874162312bd24d31b1118ae8783
Francois Botha <igitur@gmail.com>
2019-03-06 11:08:33 +0200

    Ignore _X (capital X) as XML escaping char for decoding

--- ClosedXML/Utils/XmlEncoder.cs
+++ ClosedXML/Utils/XmlEncoder.cs
@@ -7,6 +7,7 @@ namespace ClosedXML.Utils
     public static class XmlEncoder
     {
         private static readonly Regex xHHHHRegex = new Regex("_(x[\\dA-Fa-f]{4})_", RegexOptions.Compiled);
+        private static readonly Regex Uppercase_X_HHHHRegex = new Regex("_(X[\\dA-Fa-f]{4})_", RegexOptions.Compiled);
 
         public static string EncodeString(string encodeStr)
         {
@@ -33,6 +34,13 @@ namespace ClosedXML.Utils
 
         public static string DecodeString(string decodeStr)
         {
+            if (string.IsNullOrEmpty(decodeStr)) return string.Empty;
+
+            // Strings "escaped" with _X (capital X) should not be treated as escaped
+            // Example: _Xceed_Something
+            // https://github.com/ClosedXML/ClosedXML/issues/1154
+            decodeStr = Uppercase_X_HHHHRegex.Replace(decodeStr, "_x005F_$1_");
+
             return XmlConvert.DecodeName(decodeStr);
         }
     }
