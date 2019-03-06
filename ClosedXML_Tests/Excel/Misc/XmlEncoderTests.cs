6b56a58cbf848b254c9d29e6f5511af4cc3df645
Francois Botha <igitur@gmail.com>
2019-03-06 11:00:48 +0200

    Remove own implementation of `IsXmlChar`

--- ClosedXML_Tests/Excel/Misc/XmlEncoderTests.cs
+++ ClosedXML_Tests/Excel/Misc/XmlEncoderTests.cs
@@ -14,20 +14,5 @@ namespace ClosedXML_Tests.Excel
             Assert.AreEqual("\u0001 \u0002 \u0003 \u0004", XmlEncoder.DecodeString("_x0001_ _x0002_ _x0003_ _x0004_"));
             Assert.AreEqual("\u0005 \u0006 \u0007 \u0008", XmlEncoder.DecodeString("_x0005_ _x0006_ _x0007_ _x0008_"));
         }
-
-        [Test]
-        public void TestIsXmlChar()
-        {
-            Assert.AreEqual(false, XmlEncoder.IsXmlChar('\u0001'));
-            Assert.AreEqual(false, XmlEncoder.IsXmlChar('\u0005'));
-            Assert.AreEqual(false, XmlEncoder.IsXmlChar('\u0007'));
-            Assert.AreEqual(false, XmlEncoder.IsXmlChar('\u0008'));
-            Assert.AreEqual(true, XmlEncoder.IsXmlChar('J'));
-            Assert.AreEqual(true, XmlEncoder.IsXmlChar('+'));
-            Assert.AreEqual(true, XmlEncoder.IsXmlChar('S'));
-            Assert.AreEqual(true, XmlEncoder.IsXmlChar('4'));
-            Assert.AreEqual(true, XmlEncoder.IsXmlChar('!'));
-            Assert.AreEqual(true, XmlEncoder.IsXmlChar('$'));
-        }
     }
 }
