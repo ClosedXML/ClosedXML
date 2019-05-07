b5af4bae97ac960f23eb74845ba88c83f9056417
Aleksei <pankraty@gmail.com>
2019-05-07 19:03:02 +0400

    Implement XLFormulaDefinitionRepository

---
+++ ClosedXML/Excel/Formula/XLFormulaDefinitionRepository.cs
@@ -0,0 +1,38 @@
+﻿// Keep this file CodeMaid organised and cleaned
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ClosedXML.Excel.Caching;
+
+namespace ClosedXML.Excel
+{
+    internal class XLFormulaDefinitionRepository : XLRepositoryBase<XLFormulaDefinitionKey, XLFormulaDefinition>
+    {
+        #region Public Constructors
+
+        public XLFormulaDefinitionRepository(Func<XLFormulaDefinitionKey, XLFormulaDefinition> createNew)
+            : base(createNew)
+        {
+        }
+
+        public XLFormulaDefinitionRepository(Func<XLFormulaDefinitionKey, XLFormulaDefinition> createNew,
+            IEqualityComparer<XLFormulaDefinitionKey> comparer)
+            : base(createNew, comparer)
+        {
+        }
+
+        #endregion Public Constructors
+
+        public XLFormulaDefinition GetOrCreate(string formulaR1C1)
+        {
+            return GetOrCreate(new XLFormulaDefinitionKey(formulaR1C1));
+        }
+
+        public XLFormulaDefinition Store(XLFormulaDefinition formula)
+        {
+            if (formula == null) throw new ArgumentNullException(nameof(formula));
+
+            return Store(new XLFormulaDefinitionKey(formula.GetFormulaR1C1()), formula);
+        }
+    }
+}
