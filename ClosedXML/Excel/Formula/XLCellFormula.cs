3df8fee5fab6a1a77b1975799e25af1f189b364e
Aleksei <pankraty@gmail.com>
2019-05-07 19:27:16 +0400

    Implement XLCellFormula class

---
+++ ClosedXML/Excel/Formula/XLCellFormula.cs
@@ -0,0 +1,58 @@
+﻿// Keep this file CodeMaid organised and cleaned
+using System;
+
+namespace ClosedXML.Excel
+{
+    /// <summary>
+    /// A formula related to a particular cell.
+    /// </summary>
+    internal class XLCellFormula : XLContextualFormula<XLCell>
+    {
+        #region Public Constructors
+
+        public XLCellFormula(XLCell cell, XLFormulaDefinition formulaDefinition)
+            : base(cell, formulaDefinition)
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public static XLCellFormula FromFormulaA1(XLCell cell, string formulaA1)
+        {
+            if (cell == null) throw new ArgumentNullException(nameof(cell));
+
+            var repository = cell.Worksheet?.Workbook?.FormulaDefinitionRepository;
+            if (repository == null)
+                throw new InvalidOperationException("Could not get FormulaDefinitionRepository");
+
+            var formulaDefinition = new XLFormulaDefinition(formulaA1, cell.Address);
+            formulaDefinition = repository.Store(formulaDefinition);
+
+            return new XLCellFormula(cell, formulaDefinition);
+        }
+
+        public static XLCellFormula FromFormulaR1C1(XLCell cell, string formulaR1C1)
+        {
+            if (cell == null) throw new ArgumentNullException(nameof(cell));
+
+            var repository = cell.Worksheet?.Workbook?.FormulaDefinitionRepository;
+            if (repository == null)
+                throw new InvalidOperationException("Could not get FormulaDefinitionRepository");
+
+            var formulaDefinition = new XLFormulaDefinition(formulaR1C1);
+            formulaDefinition = repository.Store(formulaDefinition);
+
+            return new XLCellFormula(cell, formulaDefinition);
+        }
+
+        #endregion Public Methods
+
+        #region Overrides of XLContextualFormula
+
+        public override XLAddress BaseAddress => Context.Address;
+
+        #endregion Overrides of XLContextualFormula
+    }
+}
---
+++ ClosedXML/Excel/Formula/XLContextualFormula.cs
@@ -0,0 +1,59 @@
+﻿// Keep this file CodeMaid organised and cleaned
+using System;
+
+namespace ClosedXML.Excel
+{
+    /// <summary>
+    /// A generic version of <see cref="XLContextualFormula"/> that may relate to XLCell, XLConditionalFormat, etc.
+    /// </summary>
+    internal abstract class XLContextualFormula<T> : XLContextualFormula where T : class
+    {
+        #region Public Properties
+
+        public T Context { get; }
+
+        #endregion Public Properties
+
+        #region Public Constructors
+
+        public XLContextualFormula(T context, XLFormulaDefinition formulaDefinition) : base(formulaDefinition)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        #endregion Public Constructors
+    }
+
+    /// <summary>
+    /// A base class for storing formula related to a certain context (XLCell, for example).
+    /// The simpler name XLFormula has already been taken by another class publicly exposed,
+    /// so don't get confused.
+    /// </summary>
+    internal abstract class XLContextualFormula
+    {
+        #region Public Properties
+
+        public abstract XLAddress BaseAddress { get; }
+
+        public string FormulaA1 => _formulaDefinition.GetFormulaA1(BaseAddress);
+
+        public string FormulaR1C1 => _formulaDefinition.GetFormulaR1C1();
+
+        #endregion Public Properties
+
+        #region Protected Constructors
+
+        protected XLContextualFormula(XLFormulaDefinition formulaDefinition)
+        {
+            _formulaDefinition = formulaDefinition ?? throw new ArgumentNullException(nameof(formulaDefinition));
+        }
+
+        #endregion Protected Constructors
+
+        #region Private Fields
+
+        private readonly XLFormulaDefinition _formulaDefinition;
+
+        #endregion Private Fields
+    }
+}
