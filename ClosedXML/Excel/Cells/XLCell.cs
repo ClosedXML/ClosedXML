26861e769077b7d6717e530a09606d3ab25bcec5
Francois Botha <igitur@gmail.com>
2019-06-04 16:51:17 +0200

    Factor out default comment font

--- ClosedXML/Excel/Cells/XLCell.cs
+++ ClosedXML/Excel/Cells/XLCell.cs
@@ -161,15 +161,7 @@ namespace ClosedXML.Excel
             {
                 if (_comment == null)
                 {
-                    // MS Excel uses Tahoma 9 Swiss no matter what current style font
-                    // var style = GetStyleForRead();
-                    var defaultFont = new XLFont
-                    {
-                        FontName = "Tahoma",
-                        FontSize = 9,
-                        FontFamilyNumbering = XLFontFamilyNumberingValues.Swiss
-                    };
-                    _comment = new XLComment(this, defaultFont);
+                    _comment = new XLComment(this);
                 }
 
                 return _comment;
