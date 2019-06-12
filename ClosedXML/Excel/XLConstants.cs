d41675e199ac500c84c6bce1ea9b5122d8f807e8
Francois Botha <igitur@gmail.com>
2019-06-12 15:19:38 +0200

    Set versioning number to allow enhanced pivot table features

--- ClosedXML/Excel/XLConstants.cs
+++ ClosedXML/Excel/XLConstants.cs
@@ -1,15 +1,23 @@
+// Keep this file CodeMaid organised and cleaned
 namespace ClosedXML.Excel
 {
     //Use the class to store magic strings or variables.
     public static class XLConstants
     {
-        public const string PivotTableValuesSentinalLabel = "{{Values}}";
         public const int NumberOfBuiltInStyles = 164; // But they are stored as 0-based (0 - 163)
+
+        #region Pivot Table constants
+
+        public const byte PivotTableCreatedVersion = 5;
+        public const byte PivotTableRefreshedVersion = 5;
+        public const string PivotTableValuesSentinalLabel = "{{Values}}";
+
+        #endregion Pivot Table constants
 
         internal static class Comment
         {
-            internal const string ShapeTypeId = "_x0000_t202";
             internal const string AlternateShapeTypeId = "_xssf_cell_comment";
+            internal const string ShapeTypeId = "_x0000_t202";
         }
     }
 }
