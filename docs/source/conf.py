1cba7b39b8018d0b637eebea2ac5c5e2f2dde898
KnapSac <casper.verhaar@gmail.com>
2019-07-29 20:12:33 +0200

    Edit conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('../ClosedXML/ClosedXML/Excel'))
+sys.path.insert(0, os.path.abspath('../ClosedXML'))
 #sys.path.append( "/ext/breathe/" )
 
 
