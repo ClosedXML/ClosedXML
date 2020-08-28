c5ff5b86929419425c1da6c45cc654a71f93bfa1
Francois Botha <igitur@gmail.com>
2020-08-28 10:30:43 +0200

    Partially revert accidental change in commit c7dc2324ac6e8fd5a8a854b9ba19704f68364286

--- ClosedXML_Sandbox/PerformanceRunner.cs
+++ ClosedXML_Sandbox/PerformanceRunner.cs
@@ -42,13 +42,13 @@ namespace ClosedXML_Sandbox
 
         public static void OpenTestFile()
         {
-            var wb = new XLWorkbook();
-            var ws = wb.Worksheets.Add("Collections");
-
-            var listOfStrings = new List<String>();
-            listOfStrings.Add("House");
-            listOfStrings.Add("Car");
-            ws.Cell(1, 1).SetValue(listOfStrings);
+            using (var wb = new XLWorkbook("test.xlsx"))
+            {
+                wb.RecalculateAllFormulas();
+                var ws = wb.Worksheets.First();
+                var cell = ws.FirstCellUsed();
+                Console.WriteLine(cell.Value);
+            }
         }
 
         private static void CreateMergedCell(IXLWorksheet worksheet)
@@ -136,7 +136,7 @@ namespace ClosedXML_Sandbox
 
             return row;
         }
-        
+
         public static void PerformHeavyCalculation()
         {
             int rows = 200;
