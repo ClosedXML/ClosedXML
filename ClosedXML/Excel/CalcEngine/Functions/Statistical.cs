5050118a94afebca3d93930a6503f971353f9ff4
iamartyom <postagric@gmail.com>
2020-09-15 23:58:19 +0300

    Implement Fisher formula without use Tally class

--- ClosedXML/Excel/CalcEngine/Functions/Statistical.cs
+++ ClosedXML/Excel/CalcEngine/Functions/Statistical.cs
@@ -209,7 +209,10 @@ namespace ClosedXML.Excel.CalcEngine
 
         private static object Fisher(List<Expression> p)
         {
-            return GetTally(p, true).Fisher();
+            if (!double.TryParse(p?[0].Evaluate().ToString(), out double value)) throw new CellValueException("Parameter non numeric.");
+            if (value <= -1 || value >= 1) throw new NumberException("Incorrect value. Should be: -1 >= x <= 1.");
+
+            return 0.5 * Math.Log((1 + value) / (1 - value));
         }
 
         private static object Max(List<Expression> p)
