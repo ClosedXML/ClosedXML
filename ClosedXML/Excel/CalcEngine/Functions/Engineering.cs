bf3574859359936f6214cd219c9ac2dd4fbfeda7
iamartyom <postagric@gmail.com>
2020-09-15 05:07:04 +0300

    Add stub for engineering formulas

---
+++ ClosedXML/Excel/CalcEngine/Functions/Engineering.cs
@@ -0,0 +1,63 @@
+namespace ClosedXML.Excel.CalcEngine
+{
+    internal static class Engineering
+    {
+        public static void Register(CalcEngine ce)
+        {
+            // BESSELI Returns the modified Bessel function In(x)
+            // BESSELJ Returns the Bessel function Jn(x)
+            // BESSELK Returns the modified Bessel function Kn(x)
+            // BESSELY Returns the Bessel function Yn(x)
+            // BIN2DEC Converts a binary number to decimal
+            // BIN2HEX Converts a binary number to hexadecimal
+            // BIN2OCT Converts a binary number to octal
+            // BITAND Returns a bitwise 'And' of two numbers
+            // BITLSHIFT Returns a number shifted left by shift_amount bits
+            // BITOR Returns a bitwise 'Or' of two numbers
+            // BITRSHIFT Returns a number shifted right by shift_amount bits
+            // BITXOR Returns a bitwise 'Exclusive Or' of two numbers
+            // COMPLEX Converts real and imaginary coefficients into a complex number
+            // CONVERT Converts a number from one measurement system to another
+            // DEC2BIN Converts a decimal number to binary
+            // DEC2HEX Converts a decimal number to hexadecimal
+            // DEC2OCT Converts a decimal number to octal
+            // DELTA Tests whether two values are equal
+            // ERF Returns the error function
+            // ERF.PRECISE Returns the error function
+            // ERFC Returns the complementary error function
+            // ERFC.PRECISE Returns the complementary ERF function integrated between x and infinity
+            // GESTEP Tests whether a number is greater than a threshold value
+            // HEX2BIN Converts a hexadecimal number to binary
+            // HEX2DEC Converts a hexadecimal number to decimal
+            // HEX2OCT Converts a hexadecimal number to octal
+            // IMABS Returns the absolute value(modulus) of a complex number
+            // IMAGINARY Returns the imaginary coefficient of a complex number
+            // IMARGUMENT Returns the argument theta, an angle expressed in radians
+            // IMCONJUGATE Returns the complex conjugate of a complex number
+            // IMCOS Returns the cosine of a complex number
+            // IMCOSH Returns the hyperbolic cosine of a complex number
+            // IMCOT Returns the cotangent of a complex number
+            // IMCSC Returns the cosecant of a complex number
+            // IMCSCH Returns the hyperbolic cosecant of a complex number
+            // IMDIV Returns the quotient of two complex numbers
+            // IMEXP Returns the exponential of a complex number
+            // IMLN Returns the natural logarithm of a complex number
+            // IMLOG10 Returns the base - 10 logarithm of a complex number
+            // IMLOG2 Returns the base - 2 logarithm of a complex number
+            // IMPOWER Returns a complex number raised to an integer power
+            // IMPRODUCT Returns the product of from 2 to 255 complex numbers
+            // IMREAL Returns the real coefficient of a complex number
+            // IMSEC Returns the secant of a complex number
+            // IMSECH Returns the hyperbolic secant of a complex number
+            // IMSIN Returns the sine of a complex number
+            // IMSINH Returns the hyperbolic sine of a complex number
+            // IMSQRT Returns the square root of a complex number
+            // IMSUB Returns the difference between two complex numbers
+            // IMSUM Returns the sum of complex numbers
+            // IMTAN Returns the tangent of a complex number
+            // OCT2BIN Converts an octal number to binary
+            // OCT2DEC Converts an octal number to decimal
+            // OCT2HEX Converts an octal number to hexadecimal
+        }
+    }
+}
