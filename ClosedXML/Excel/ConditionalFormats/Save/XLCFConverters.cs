9ad135f0265b3e9bbd802d718c0af64cb84df438
Aleksei <pankraty@gmail.com>
2020-09-18 18:31:18 +0400

    Refactor dictionary initialization and access

--- ClosedXML/Excel/ConditionalFormats/Save/XLCFConverters.cs
+++ ClosedXML/Excel/ConditionalFormats/Save/XLCFConverters.cs
@@ -1,8 +1,6 @@
+using DocumentFormat.OpenXml.Spreadsheet;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace ClosedXML.Excel
 {
@@ -11,36 +9,31 @@ namespace ClosedXML.Excel
         private static readonly Dictionary<XLConditionalFormatType, IXLCFConverter> Converters;
         static XLCFConverters()
         {
-            Converters = new Dictionary<XLConditionalFormatType, IXLCFConverter>();
-            Converters.Add(XLConditionalFormatType.ColorScale, new XLCFColorScaleConverter());
-            Converters.Add(XLConditionalFormatType.StartsWith, new XLCFStartsWithConverter());
-            Converters.Add(XLConditionalFormatType.EndsWith, new XLCFEndsWithConverter());
-            Converters.Add(XLConditionalFormatType.IsBlank, new XLCFIsBlankConverter());
-            Converters.Add(XLConditionalFormatType.NotBlank, new XLCFNotBlankConverter());
-            Converters.Add(XLConditionalFormatType.IsError, new XLCFIsErrorConverter());
-            Converters.Add(XLConditionalFormatType.NotError, new XLCFNotErrorConverter());
-            Converters.Add(XLConditionalFormatType.ContainsText, new XLCFContainsConverter());
-            Converters.Add(XLConditionalFormatType.NotContainsText, new XLCFNotContainsConverter());
-            Converters.Add(XLConditionalFormatType.CellIs, new XLCFCellIsConverter());
-            Converters.Add(XLConditionalFormatType.IsUnique, new XLCFUniqueConverter());
-            Converters.Add(XLConditionalFormatType.IsDuplicate, new XLCFUniqueConverter());
-            Converters.Add(XLConditionalFormatType.Expression, new XLCFCellIsConverter());
-            Converters.Add(XLConditionalFormatType.Top10, new XLCFTopConverter());
-            Converters.Add(XLConditionalFormatType.DataBar, new XLCFDataBarConverter());
-            Converters.Add(XLConditionalFormatType.IconSet, new XLCFIconSetConverter());
-            Converters.Add(XLConditionalFormatType.TimePeriod, new XLCFDatesOccurringConverter());
-
-            foreach (var cft in Enum.GetValues(typeof(XLConditionalFormatType)).Cast<XLConditionalFormatType>())
+            Converters = new Dictionary<XLConditionalFormatType, IXLCFConverter>
             {
-                if (!Converters.ContainsKey(cft))
-                    Converters.Add(cft, null);
-            }
+                {XLConditionalFormatType.ColorScale, new XLCFColorScaleConverter()},
+                {XLConditionalFormatType.StartsWith, new XLCFStartsWithConverter()},
+                {XLConditionalFormatType.EndsWith, new XLCFEndsWithConverter()},
+                {XLConditionalFormatType.IsBlank, new XLCFIsBlankConverter()},
+                {XLConditionalFormatType.NotBlank, new XLCFNotBlankConverter()},
+                {XLConditionalFormatType.IsError, new XLCFIsErrorConverter()},
+                {XLConditionalFormatType.NotError, new XLCFNotErrorConverter()},
+                {XLConditionalFormatType.ContainsText, new XLCFContainsConverter()},
+                {XLConditionalFormatType.NotContainsText, new XLCFNotContainsConverter()},
+                {XLConditionalFormatType.CellIs, new XLCFCellIsConverter()},
+                {XLConditionalFormatType.IsUnique, new XLCFUniqueConverter()},
+                {XLConditionalFormatType.IsDuplicate, new XLCFUniqueConverter()},
+                {XLConditionalFormatType.Expression, new XLCFCellIsConverter()},
+                {XLConditionalFormatType.Top10, new XLCFTopConverter()},
+                {XLConditionalFormatType.DataBar, new XLCFDataBarConverter()},
+                {XLConditionalFormatType.IconSet, new XLCFIconSetConverter()},
+                {XLConditionalFormatType.TimePeriod, new XLCFDatesOccurringConverter()}
+            };
         }
 
         public static ConditionalFormattingRule Convert(IXLConditionalFormat conditionalFormat, Int32 priority, XLWorkbook.SaveContext context)
         {
-            var converter = Converters[conditionalFormat.ConditionalFormatType];
-            if (converter == null)
+            if (!Converters.TryGetValue(conditionalFormat.ConditionalFormatType, out var converter))
                 throw new NotImplementedException(string.Format("Conditional formatting rule '{0}' hasn't been implemented", conditionalFormat.ConditionalFormatType));
 
             return converter.Convert(conditionalFormat, priority, context);
