5e07ce8627769d9e4b51f5d51d0d01bccf4168cf
Francois Botha <igitur@gmail.com>
2020-09-21 19:11:41 +0200

    Clean up XLHelperTests

--- ClosedXML_Tests/XLHelperTests.cs
+++ ClosedXML_Tests/XLHelperTests.cs
@@ -3,50 +3,9 @@ using NUnit.Framework;
 
 namespace ClosedXML_Tests
 {
-    /// <summary>
-    ///     This is a test class for XLHelperTests and is intended
-    ///     to contain all XLHelperTests Unit Tests
-    /// </summary>
     [TestFixture]
     public class XLHelperTests
     {
-        /// <summary>
-        ///     Gets or sets the test context which provides
-        ///     information about and functionality for the current test run.
-        /// </summary>
-        public TestContext TestContext { get; set; }
-
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-
-        /// <summary>
-        ///     A test for IsValidColumn
-        /// </summary>
         [Test]
         public void IsValidColumnTest()
         {
