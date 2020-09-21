672909ae9cfa0a6c474d24b340430f76ca7f56ea
Christoph Wüthrich <c.wuethrich@gmail.com>
2020-09-21 22:50:05 +0200

    use XLStylizedEmpty in pivot table
    At least don't throw an exception.

--- ClosedXML.Tests/Excel/Loading/LoadingTests.cs
+++ ClosedXML.Tests/Excel/Loading/LoadingTests.cs
@@ -174,6 +174,17 @@ namespace ClosedXML.Tests.Excel
             }
         }
 
+        [Test]
+        public void CanLoadPivotTableWithBorder()
+        {
+            using (var stream = TestHelper.GetStreamFromResource(TestHelper.GetResourcePath(@"TryToLoad\PivotTableWithBorder.xlsx")))
+            using (var wb = new XLWorkbook(stream))
+            {
+                Assert.IsNotNull(wb);
+                //TODO: Implement pivot table with border
+            }
+        }
+
         /// <summary>
         /// For non-English locales, the default style ("Normal" in English) can be
         /// another piece of text (e.g. ??????? in Russian).
