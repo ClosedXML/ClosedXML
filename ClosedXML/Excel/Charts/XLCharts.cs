bcfd27216bdbedc74856cffa3b85ed8a7477ab4b
steffi3011 <72965123+steffi3011@users.noreply.github.com>
2020-10-19 09:20:50 +0200

    Update XLCharts.cs

--- ClosedXML/Excel/Charts/XLCharts.cs
+++ ClosedXML/Excel/Charts/XLCharts.cs
@@ -2,9 +2,16 @@ using System.Collections.Generic;
 
 namespace ClosedXML.Excel
 {
-    internal class XLCharts: IXLCharts
+    public class XLCharts : IXLCharts
     {
         private List<IXLChart> charts = new List<IXLChart>();
+        private readonly IXLWorksheet _worksheet;
+
+        public XLCharts(IXLWorksheet worksheet)
+        {
+            _worksheet = worksheet;
+        }
+
         public IEnumerator<IXLChart> GetEnumerator()
         {
             return charts.GetEnumerator();
@@ -15,9 +22,15 @@ namespace ClosedXML.Excel
             return GetEnumerator();
         }
 
-        public void Add(IXLChart chart)
+        public IXLChart Chart(int index)
+        {
+            return charts[index];
+        }
+
+        public IXLChart Add(IXLChart chart)
         {
             charts.Add(chart);
+            return chart;
         }
     }
 }
