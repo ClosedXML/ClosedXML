67b70d101f03fb28c5c460a3422d2a3fa8a53a3e
Roman Brandstetter <roman.brandstetter@rubicon.eu>
2020-12-07 17:07:45 +0100

    fix test #1575
    (cherry picked from commit 3613eb170e88aa8aa09c23a7c6955717ea2654eb)

--- ClosedXML_Tests/Excel/Cells/XLCellTests.cs
+++ ClosedXML_Tests/Excel/Cells/XLCellTests.cs
@@ -387,7 +387,7 @@ namespace ClosedXML_Tests
         [TestCase(XLCellsUsedOptions.DataType, false)]
         [TestCase(XLCellsUsedOptions.NormalFormats, true)]
         [TestCase(XLCellsUsedOptions.ConditionalFormats, true)]
-        [TestCase(XLCellsUsedOptions.Comments, false)]
+        [TestCase(XLCellsUsedOptions.Comments, true)]
         [TestCase(XLCellsUsedOptions.DataValidation, true)]
         [TestCase(XLCellsUsedOptions.MergedRanges, true)]
         [TestCase(XLCellsUsedOptions.Sparklines, true)]
