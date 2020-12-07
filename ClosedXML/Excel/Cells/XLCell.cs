5ade148102d4afa7ed3001ab5d1e10490dab480c
Roman Brandstetter <roman.brandstetter@rubicon.eu>
2020-12-07 16:03:42 +0100

    considering options when checking for Empty cell #1575

--- ClosedXML/Excel/Cells/XLCell.cs
+++ ClosedXML/Excel/Cells/XLCell.cs
@@ -1520,8 +1520,11 @@ namespace ClosedXML.Excel
 
         public Boolean IsEmpty(XLCellsUsedOptions options)
         {
-            if (InnerText.Length > 0)
-                return false;
+            if (options.HasFlag (XLCellsUsedOptions.Contents))
+            {
+                if (InnerText.Length > 0)
+                    return false;
+            }
 
             if (options.HasFlag(XLCellsUsedOptions.NormalFormats))
             {
