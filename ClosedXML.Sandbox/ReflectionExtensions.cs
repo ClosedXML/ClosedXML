b8b58d1d5fae841c844cbe77ea3e498b33308385
Francois Botha <igitur@gmail.com>
2021-03-04 12:23:56 +0200

    Rename ClosedXML_Sandbox to ClosedXML.Sandbox

--- ClosedXML.Sandbox/ReflectionExtensions.cs
+++ ClosedXML.Sandbox/ReflectionExtensions.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 
-namespace ClosedXML_Sandbox
+namespace ClosedXML.Sandbox
 {
     internal static class ReflectionExtensions
     {
