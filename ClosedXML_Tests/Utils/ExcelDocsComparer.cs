e53223f148cb1afa475f6c3679398fa7381ce8cb
Stefan Seeland <stefan.seeland@rubicon.eu>
2021-03-16 19:11:09 +0100

    Bump DocumentFormat.OpenXml from 2.7.2 to 2.11.3

--- ClosedXML_Tests/Utils/ExcelDocsComparer.cs
+++ ClosedXML_Tests/Utils/ExcelDocsComparer.cs
@@ -8,8 +8,8 @@ namespace ClosedXML_Tests
     {
         public static bool Compare(string left, string right, out string message)
         {
-            using (FileStream leftStream = File.OpenRead(left))
-            using (FileStream rightStream = File.OpenRead(right))
+            using (var leftStream = File.OpenRead(left))
+            using (var rightStream = File.OpenRead(right))
             {
                 return Compare(leftStream, rightStream, out message);
             }
@@ -17,8 +17,8 @@ namespace ClosedXML_Tests
 
         public static bool Compare(Stream left, Stream right, out string message)
         {
-            using (Package leftPackage = Package.Open(left, FileMode.Open, FileAccess.Read))
-            using (Package rightPackage = Package.Open(right, FileMode.Open, FileAccess.Read))
+            using (var leftPackage = Package.Open(left, FileMode.Open, FileAccess.Read))
+            using (var rightPackage = Package.Open(right, FileMode.Open, FileAccess.Read))
             {
                 return PackageHelper.Compare(leftPackage, rightPackage, false, ExcludeMethod, out message);
             }
@@ -28,7 +28,9 @@ namespace ClosedXML_Tests
         {
             //Exclude service data
             if (uri.OriginalString.EndsWith(".rels") ||
-                uri.OriginalString.EndsWith(".psmdcp"))
+                uri.OriginalString.EndsWith(".psmdcp") ||
+                uri.OriginalString.EndsWith("app.xml"))
+
             {
                 return true;
             }
