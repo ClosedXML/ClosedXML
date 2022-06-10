e4cfc45f6c2b9095f634a1d5a69bf530f631a51a
Martin <schultz-martin@web.de>
2022-06-10 19:27:48 +0200

    Revert "Auxiliary commit to revert individual files from 5913132d1072d3d50b6bde5650458c988d7683f8"
    This reverts commit eeea09f3c480cb0399cace360ba86f1498a0fb27.

--- ClosedXML.Tests/Excel/Misc/XLWorkbookTests.cs
+++ ClosedXML.Tests/Excel/Misc/XLWorkbookTests.cs
@@ -359,7 +359,7 @@ namespace ClosedXML.Tests.Excel
             using (var wb = new XLWorkbook())
             {
                 var ws = wb.Worksheets.Add("Sheet1");
-                wb.Protect(password: String.Empty);
+                wb.Protect(true, false);
                 Assert.IsTrue(wb.LockStructure);
                 Assert.IsFalse(wb.LockWindows);
                 Assert.IsFalse(wb.IsPasswordProtected);
@@ -406,7 +406,7 @@ namespace ClosedXML.Tests.Excel
             using (var wb = new XLWorkbook())
             {
                 var ws = wb.Worksheets.Add("Sheet1");
-                wb.Protect("Abc@123");
+                wb.Protect(true, false, "Abc@123");
                 Assert.IsTrue(wb.LockStructure);
                 Assert.IsFalse(wb.LockWindows);
                 Assert.IsTrue(wb.IsPasswordProtected);
