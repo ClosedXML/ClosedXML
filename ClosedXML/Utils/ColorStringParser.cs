9141d2e18c3e6f8cac3bfadc9f625e84b7379675
jahav <havlicek.honza@gmail.com>
2022-09-27 11:55:33 +0200

    Remove a dependency on System.Drawing.Common by using a compatible color parser.

--- ClosedXML/Utils/ColorStringParser.cs
+++ ClosedXML/Utils/ColorStringParser.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
 using System.Globalization;
 
 namespace ClosedXML.Utils
@@ -9,10 +11,26 @@ namespace ClosedXML.Utils
         {
             try
             {
-                if (htmlColor[0] != '#')
-                    htmlColor = '#' + htmlColor;
+                if (htmlColor[0] == '#' && (htmlColor.Length == 4 || htmlColor.Length == 7))
+                {
+                    if (htmlColor.Length == 4)
+                    {
+                        var r = ReadHex(htmlColor, 1, 1);
+                        var g = ReadHex(htmlColor, 2, 1);
+                        var b = ReadHex(htmlColor, 3, 1);
+                        return Color.FromArgb(
+                            (r << 4) | r,
+                            (g << 4) | g,
+                            (b << 4) | b);
+                    }
 
-                return ColorTranslator.FromHtml(htmlColor);
+                    return Color.FromArgb(
+                        ReadHex(htmlColor, 1, 2),
+                        ReadHex(htmlColor, 3, 2),
+                        ReadHex(htmlColor, 5, 2));
+                }
+
+                return (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(htmlColor);
             }
             catch
             {
@@ -20,6 +38,11 @@ namespace ClosedXML.Utils
                 // When regional settings list separator is # , the standard ColorTranslator.FromHtml fails
                 return Color.FromArgb(int.Parse(htmlColor.Replace("#", ""), NumberStyles.AllowHexSpecifier));
             }
+        }
+
+        private static int ReadHex(string text, int start, int length)
+        {
+            return Convert.ToInt32(text.Substring(start, length), 16);
         }
     }
 }
