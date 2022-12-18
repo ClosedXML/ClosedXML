748ac842d9e4c813e0adb0fd4cb484cb4d946871
jahav <havlicek.honza@gmail.com>
2022-12-18 03:38:52 +0100

    1/3 Saving SharedStringTable through streaming: Move the code for writing shared string table to a separate writer. Part of a move to 2 phase saving and DOM-less saving though streaming of elements.

---
+++ ClosedXML/Excel/IO/SharedStringTableWriter.cs
@@ -0,0 +1,83 @@
+﻿using ClosedXML.Utils;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static ClosedXML.Excel.XLWorkbook;
+
+namespace ClosedXML.Excel.IO
+{
+    internal class SharedStringTableWriter
+    {
+        internal static void GenerateSharedStringTablePartContent(XLWorkbook workbook, SharedStringTablePart sharedStringTablePart,
+            SaveContext context)
+        {
+            // Call all table headers to make sure their names are filled
+            var x = 0;
+            workbook.Worksheets.ForEach(w => w.Tables.ForEach(t => x = (t as XLTable).FieldNames.Count));
+
+            sharedStringTablePart.SharedStringTable = new SharedStringTable { Count = 0, UniqueCount = 0 };
+
+            var stringId = 0;
+
+            var newStrings = new Dictionary<String, Int32>();
+            var newRichStrings = new Dictionary<IXLRichText, Int32>();
+
+            static bool HasSharedString(XLCell c)
+            {
+                if (c.DataType == XLDataType.Text && c.ShareString)
+                    return c.StyleValue.IncludeQuotePrefix || String.IsNullOrWhiteSpace(c.FormulaA1) && c.GetText().Length > 0;
+                else
+                    return false;
+            }
+
+            foreach (var c in workbook.Worksheets.Cast<XLWorksheet>().SelectMany(w => w.Internals.CellsCollection.GetCells(HasSharedString)))
+            {
+                if (c.HasRichText)
+                {
+                    if (newRichStrings.TryGetValue(c.GetRichText(), out int id))
+                        c.SharedStringId = id;
+                    else
+                    {
+                        var sharedStringItem = new SharedStringItem();
+                        TextSerializer.PopulatedRichTextElements(sharedStringItem, c, context);
+
+                        sharedStringTablePart.SharedStringTable.Append(sharedStringItem);
+                        sharedStringTablePart.SharedStringTable.Count += 1;
+                        sharedStringTablePart.SharedStringTable.UniqueCount += 1;
+
+                        newRichStrings.Add(c.GetRichText(), stringId);
+                        c.SharedStringId = stringId;
+
+                        stringId++;
+                    }
+                }
+                else
+                {
+                    var value = c.Value.GetText();
+                    if (newStrings.TryGetValue(value, out int id))
+                        c.SharedStringId = id;
+                    else
+                    {
+                        var s = value;
+                        var sharedStringItem = new SharedStringItem();
+                        var text = new Text { Text = XmlEncoder.EncodeString(s) };
+                        if (!s.Trim().Equals(s))
+                            text.Space = SpaceProcessingModeValues.Preserve;
+                        sharedStringItem.Append(text);
+                        sharedStringTablePart.SharedStringTable.Append(sharedStringItem);
+                        sharedStringTablePart.SharedStringTable.Count += 1;
+                        sharedStringTablePart.SharedStringTable.UniqueCount += 1;
+
+                        newStrings.Add(value, stringId);
+                        c.SharedStringId = stringId;
+
+                        stringId++;
+                    }
+                }
+            }
+        }
+    }
+}
