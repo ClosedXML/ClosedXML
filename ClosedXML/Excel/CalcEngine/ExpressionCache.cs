a99871a7d3a906cf9616ca0cee1a9d5b56e7510f
Marko Lahma <marko.lahma@gmail.com>
2023-01-12 10:59:57 +0200

    Change ExpressionCache to use ConditionalWeakTable

--- ClosedXML/Excel/CalcEngine/ExpressionCache.cs
+++ ClosedXML/Excel/CalcEngine/ExpressionCache.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace ClosedXML.Excel.CalcEngine
 {
@@ -10,16 +9,15 @@ namespace ClosedXML.Excel.CalcEngine
     /// <remarks>
     /// Uses weak references to avoid accumulating unused expressions.
     /// </remarks>
-    class ExpressionCache
+    internal sealed class ExpressionCache
     {
-        Dictionary<string, WeakReference> _dct;
-        CalcEngine _ce;
-        int _hitCount;
+        private readonly ConditionalWeakTable<string, Formula> _cache;
+        private readonly CalcEngine _ce;
 
         public ExpressionCache(CalcEngine ce)
         {
             _ce = ce;
-            _dct = new Dictionary<string, WeakReference>();
+            _cache = new ConditionalWeakTable<string, Formula>();
         }
 
         // gets the parsed version of a string expression
@@ -27,43 +25,12 @@ namespace ClosedXML.Excel.CalcEngine
         {
             get
             {
-                Formula x;
-                if (_dct.TryGetValue(expression, out WeakReference wr) && wr.IsAlive)
+                if (!_cache.TryGetValue(expression, out var formula))
                 {
-                    x = wr.Target as Formula;
+                    formula = _ce.Parse(expression);
+                    _cache.Add(expression, formula);
                 }
-                else
-                {
-                    // remove all dead references from dictionary
-                    if (wr != null && _dct.Count > 100 && _hitCount++ > 100)
-                    {
-                        RemoveDeadReferences();
-                        _hitCount = 0;
-                    }
-
-                    // store this expression
-                    x = _ce.Parse(expression);
-                    _dct[expression] = new WeakReference(x);
-                }
-                return x;
-            }
-        }
-
-        // remove all dead references from the cache
-        void RemoveDeadReferences()
-        {
-            for (bool done = false; !done; )
-            {
-                done = true;
-                foreach (var k in _dct.Keys)
-                {
-                    if (!_dct[k].IsAlive)
-                    {
-                        _dct.Remove(k);
-                        done = false;
-                        break;
-                    }
-                }
+                return formula;
             }
         }
     }
