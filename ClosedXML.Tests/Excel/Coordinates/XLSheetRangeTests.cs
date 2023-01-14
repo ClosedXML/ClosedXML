ed7e98b90b75c73772712f7170c457ac84c497a1
jahav <havlicek.honza@gmail.com>
2023-01-14 20:06:10 +0100

    XLCellFormula separation 25/27 - Add parsing logic for ST_Ref to XLSheetRange. The ST_Ref is used extensively during loading. It is different from the one in XLAddressRange, that one parses references with worksheets and can be invalid.

---
+++ ClosedXML.Tests/Excel/Coordinates/XLSheetRangeTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ClosedXML.Excel;
+using NUnit.Framework;
+
+namespace ClosedXML.Tests.Excel.Coordinates
+{
+    [TestFixture]
+    public class XLSheetRangeTests
+    {
+        [TestCase("A1", 1, 1, 1, 1)]
+        [TestCase("A1:Z100", 1, 1, 100, 26)]
+        [TestCase("BD14:EG256", 14, 56, 256, 137)]
+        [TestCase("A1:XFD1048576", 1, 1, 1048576, 16384)]
+        [TestCase("XFD1048576", 1048576, 16384, 1048576, 16384)]
+        [TestCase("XFD1048576:XFD1048576", 1048576, 16384, 1048576, 16384)]
+        public void ParseCellRefsAccordingToGrammar(string refText, int firstRow, int firstCol, int lastRow, int lastCol)
+        {
+            var reference = XLSheetRange.Parse(refText);
+            Assert.AreEqual(firstRow, reference.FirstPoint.Row);
+            Assert.AreEqual(firstCol, reference.FirstPoint.Column);
+            Assert.AreEqual(lastRow, reference.LastPoint.Row);
+            Assert.AreEqual(lastCol, reference.LastPoint.Column);
+        }
+
+        [TestCase("")]
+        [TestCase("A1:")]
+        [TestCase(":A1")]
+        [TestCase("A1: A1")]
+        [TestCase(" A1:A1")]
+        [TestCase("A1:A1 ")]
+        [TestCase("B1:A1")]
+        [TestCase("A2:A1")]
+        public void InvalidInputsAreNotParsed(string invalidRef)
+        {
+            Assert.Throws<FormatException>(() => XLSheetRange.Parse(invalidRef));
+        }
+    }
+}
--- ClosedXML/Excel/Coordinates/XLSheetRange.cs
+++ ClosedXML/Excel/Coordinates/XLSheetRange.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ClosedXML.Excel
 {
-    internal struct XLSheetRange:IEquatable<XLSheetRange>
+    internal struct XLSheetRange : IEquatable<XLSheetRange>
     {
         public XLSheetRange(XLSheetPoint firstPoint, XLSheetPoint lastPoint)
         {
@@ -21,6 +21,33 @@ namespace ClosedXML.Excel
         public override int GetHashCode()
         {
             return FirstPoint.GetHashCode() ^ LastPoint.GetHashCode();
+        }
+
+        /// <inheritdoc cref="Parse(ReadOnlySpan{char})"/>
+        public static XLSheetRange Parse(String input) => Parse(input.AsSpan());
+
+        /// <summary>
+        /// Parse point per type <c>ST_Ref</c> from
+        /// <a href="https://learn.microsoft.com/en-us/openspecs/office_standards/ms-oe376/e7f22870-88a1-4c06-8e5f-d035b1179c50">2.1.1119 Part 4 Section 3.18.64, ST_Ref (Cell Range Reference)</a>
+        /// </summary>
+        /// <remarks>Can be one cell reference (A1) or two separated by a colon (A1:B2). First reference is always in top left corner</remarks>
+        /// <param name="input">Input text</param>
+        /// <exception cref="FormatException">If the input doesn't match expected grammar.</exception>
+        public static XLSheetRange Parse(ReadOnlySpan<char> input)
+        {
+            var separatorIndex = input.IndexOf(':');
+            if (separatorIndex == -1)
+            {
+                var sheetPoint = XLSheetPoint.Parse(input);
+                return new XLSheetRange(sheetPoint, sheetPoint);
+            }
+
+            var first = XLSheetPoint.Parse(input.Slice(0, separatorIndex));
+            var second = XLSheetPoint.Parse(input.Slice(separatorIndex + 1, input.Length - separatorIndex - 1));
+            if (first.Column > second.Column || first.Row > second.Row)
+                throw new FormatException($"First reference must have smaller column and row ('{input.ToString()}')");
+
+            return new XLSheetRange(first, second);
         }
     }
 }
