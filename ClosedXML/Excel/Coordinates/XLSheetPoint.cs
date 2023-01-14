d267606e1d3a2eba7c79648f6c0619d1b2a0f072
jahav <havlicek.honza@gmail.com>
2023-01-14 19:11:42 +0100

    XLCellFormula separation 24/27 - Add parsing logic for ST_CellRef to XLSheetPoint. The ST_CellRef is used extensively during loading. XLAddress is not suitable, it contains a worksheet and parses references.

--- ClosedXML/Excel/Coordinates/XLSheetPoint.cs
+++ ClosedXML/Excel/Coordinates/XLSheetPoint.cs
@@ -3,7 +3,7 @@ using System;
 namespace ClosedXML.Excel
 {
     /// <summary>
-    /// An point (address) in a worksheet.
+    /// An point (address) in a worksheet, an equivalent of <c>ST_CellRef</c>.
     /// </summary>
     /// <remarks>Unlike the XLAddress, sheet can never be invalid.</remarks>
     internal readonly struct XLSheetPoint : IEquatable<XLSheetPoint>
@@ -47,6 +47,65 @@ namespace ClosedXML.Excel
         public static bool operator !=(XLSheetPoint a, XLSheetPoint b)
         {
             return a.Row != b.Row || a.Column != b.Column;
+        }
+
+        /// <inheritdoc cref="Parse(ReadOnlySpan{char})"/>
+        public static XLSheetPoint Parse(String text) => Parse(text.AsSpan());
+
+        /// <summary>
+        /// Parse point per type <c>ST_CellRef</c> from
+        /// <a href="https://learn.microsoft.com/en-us/openspecs/office_standards/ms-oe376/db11a912-b1cb-4dff-b46d-9bedfd10cef0">2.1.1108 Part 4 Section 3.18.8, ST_CellRef (Cell Reference)</a>
+        /// </summary>
+        /// <param name="input">Input text</param>
+        /// <exception cref="FormatException">If the input doesn't match expected grammar.</exception>
+        public static XLSheetPoint Parse(ReadOnlySpan<char> input)
+        {
+            // Don't reuse inefficient logic from XLAddress
+            if (input.Length < 2)
+                throw new FormatException($"Length is less than two ('{input.ToString()}').");
+
+            var i = 0;
+            var c = input[i++];
+            if (!IsLetter(c))
+                throw new FormatException($"Doesn't start with a letter ('{input.ToString()}').");
+
+            var columnIndex = c - 'A' + 1;
+            while (i < input.Length && IsLetter(c = input[i]))
+            {
+                columnIndex = columnIndex * 26 + c - 'A' + 1;
+                i++;
+            }
+
+            if (i > 3)
+                throw new FormatException($"Input contains more than three letters ('{input.ToString()}').");
+
+            if (i == input.Length)
+                throw new FormatException($"Input doesn't contain row number ('{input.ToString()}').");
+
+            // Everything else must be digits
+            c = input[i++];
+
+            // First letter can't be 0
+            if (c is < '1' or > '9')
+                throw new FormatException($"Row must start with a non-zero digit ('{input.ToString()}').");
+
+            var rowIndex = c - '0';
+            while (i < input.Length && IsDigit(c = input[i]))
+            {
+                rowIndex = rowIndex * 10 + c - '0';
+                i++;
+            }
+
+            if (i != input.Length)
+                throw new FormatException($"Input contains unexpected characters ('{input.ToString()}').");
+
+            if (rowIndex > XLHelper.MaxRowNumber || columnIndex > XLHelper.MaxColumnNumber)
+                throw new FormatException($"Address out of bounds ('{input.ToString()}').");
+
+            return new XLSheetPoint(rowIndex, columnIndex);
+
+            static bool IsLetter(char c) => c is >= 'A' and <= 'Z';
+            static bool IsDigit(char c) => c is >= '0' and <= '9';
         }
     }
 }
