f053aa24b963cfd49fd60ef751e0d79e17084ba8
jahav <havlicek.honza@gmail.com>
2023-01-14 19:11:42 +0100

    XLCellFormula separation 24/27 - Add parsing logic for ST_CellRef to XLSheetPoint. The ST_CellRef is used extensively during loading. XLAddress is not suitable, it contains a worksheet and parses references.

---
+++ ClosedXML.Tests/Excel/Coordinates/XLSheetPointTests.cs
@@ -0,0 +1,54 @@
+﻿using ClosedXML.Excel;
+using NUnit.Framework;
+using System;
+
+namespace ClosedXML.Tests.Excel.Coordinates
+{
+    [TestFixture]
+    public class XLSheetPointTests
+    {
+        [TestCase("A1", 1, 1)]
+        [TestCase("AA1", 27, 1)]
+        [TestCase("AAA1", 703, 1)]
+        [TestCase("Z1", 26, 1)]
+        [TestCase("ZZ1", 702, 1)]
+        [TestCase("XFD1", 16384, 1)]
+        [TestCase("A1", 1, 1)]
+        [TestCase("A999", 1, 999)]
+        [TestCase("XFD1048576", 16384, 1048576)]
+        public void ParseCellRefsAccordingToGrammar(string cellRef, int columnNumber, int rowNumber)
+        {
+            var sheetPoint = XLSheetPoint.Parse(cellRef.AsSpan());
+            Assert.AreEqual(columnNumber, sheetPoint.Column);
+            Assert.AreEqual(rowNumber, sheetPoint.Row);
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("A")]
+        [TestCase("AA")]
+        [TestCase("1")]
+        [TestCase("11")]
+        [TestCase(" A1")]
+        [TestCase("A1 ")]
+        [TestCase("A 1")]
+        [TestCase("@1")] // @ is a char 'A' - 1
+        [TestCase("[1")] // [ is a char 'Z' + 1
+        [TestCase("A:")] // : is a char '9' + 1
+        [TestCase("A/")] // / is a char '0' - 1
+        [TestCase("A1:")]
+        [TestCase("A1/")]
+        [TestCase("A@1")]
+        [TestCase("A[1")]
+        [TestCase("XFE1")]
+        [TestCase("AAAA1")]
+        [TestCase("A1048577")]
+        [TestCase("A01")]
+        [TestCase("A0")]
+        [TestCase("A-1")]
+        public void InvalidInputsAreNotParsed(string cellRef)
+        {
+            Assert.Throws<FormatException>(() => XLSheetPoint.Parse(cellRef.AsSpan()));
+        }
+    }
+}
