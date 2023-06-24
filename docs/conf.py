c3c8529a788067c9e5c03991986476921dd88b9f
jahav <havlicek.honza@gmail.com>
2023-06-24 17:48:26 +0200

    Bump version to 0.102.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../ClosedXML'))
 project = 'ClosedXML'
 
 # The full version, including alpha/beta/rc tags
-release = '0.97.1-preview'
+release = '0.102.0'
 
 # -- General configuration ---------------------------------------------------
 
