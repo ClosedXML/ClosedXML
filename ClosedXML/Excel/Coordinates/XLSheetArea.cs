c84006504387f2cfe317a960ccf96839a3e9c5be
jahav <havlicek.honza@gmail.com>
2023-08-13 20:41:57 +0200

    Add a structure to hold info about a reference to an area in a sheet.

---
+++ ClosedXML/Excel/Coordinates/XLSheetArea.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClosedXML.Excel
+{
+    /// <summary>
+    /// A specification of an area (rectangular range) of a sheet.
+    /// </summary>
+    internal readonly struct XLSheetArea : IEquatable<XLSheetArea>
+    {
+        /// <summary>
+        /// Name of the sheet. Sheet may exist or not.
+        /// </summary>
+        public readonly string Name;
+
+        /// <summary>
+        /// An area in the sheet.
+        /// </summary>
+        public readonly XLSheetRange Area;
+
+        public XLSheetArea(String name, XLSheetRange area)
+        {
+            Name = name;
+            Area = area;
+        }
+
+        public bool Equals(XLSheetArea other)
+        {
+            return Area == other.Area && StringComparer.OrdinalIgnoreCase.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is XLSheetArea other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Name.GetHashCode() * 397) ^ Area.GetHashCode();
+            }
+        }
+    }
+}
