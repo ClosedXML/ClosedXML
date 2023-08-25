153768f440d0102aed7cc508b98b6e5fe89a92da
jahav <havlicek.honza@gmail.com>
2023-08-25 16:59:18 +0200

    Add intersection operation to XLSheetArea. Unlike XLSheetRange, this one also includes comparison of a sheet, so it is more general use.

---
+++ ClosedXML.Tests/Excel/Coordinates/XLSheetAreaTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+
+namespace ClosedXML.Tests.Excel.Coordinates
+{
+    [TestFixture]
+    public class XLSheetAreaTests
+    {
+        [Test]
+        public void Equality_uses_case_insensitive_comparison_for_sheet_name()
+        {
+            var upperCase = new XLSheetArea("NAME", new XLSheetRange(1, 2, 3, 4));
+            var lowerCase = new XLSheetArea("name", new XLSheetRange(1, 2, 3, 4));
+            Assert.AreEqual(upperCase.GetHashCode(), lowerCase.GetHashCode());
+            Assert.AreEqual(upperCase, lowerCase);
+        }
+
+        [Test]
+        public void Intersection_produces_range_intersection_in_same_sheet()
+        {
+            var left = new XLSheetArea("THIS", XLSheetRange.Parse("A1:C3"));
+            var rightSameSheet = new XLSheetArea("this", XLSheetRange.Parse("B2:D4"));
+            var rightDifferentSheet = new XLSheetArea("Different", XLSheetRange.Parse("B2:D4"));
+
+            var sameSheetIntersection = left.Intersect(rightSameSheet);
+            Assert.AreEqual(new XLSheetArea("this", XLSheetRange.Parse("B2:C3")), sameSheetIntersection);
+
+            var differentSheetIntersection = left.Intersect(rightDifferentSheet);
+            Assert.Null(differentSheetIntersection);
+        }
+    }
+}
--- ClosedXML/Excel/Coordinates/XLSheetArea.cs
+++ ClosedXML/Excel/Coordinates/XLSheetArea.cs
@@ -8,7 +8,7 @@ namespace ClosedXML.Excel
     internal readonly struct XLSheetArea : IEquatable<XLSheetArea>
     {
         /// <summary>
-        /// Name of the sheet. Sheet may exist or not.
+        /// Name of the sheet. Sheet may exist or not (e.g. deleted).
         /// </summary>
         public readonly string Name;
 
@@ -25,7 +25,7 @@ namespace ClosedXML.Excel
 
         public bool Equals(XLSheetArea other)
         {
-            return Area == other.Area && StringComparer.OrdinalIgnoreCase.Equals(Name, other.Name);
+            return Area == other.Area && XLHelper.SheetComparer.Equals(Name, other.Name);
         }
 
         public override bool Equals(object? obj)
@@ -37,8 +37,26 @@ namespace ClosedXML.Excel
         {
             unchecked
             {
-                return (Name.GetHashCode() * 397) ^ Area.GetHashCode();
+               
+                return (XLHelper.SheetComparer.GetHashCode(Name) * 397) ^ Area.GetHashCode();
             }
+        }
+
+        /// <summary>
+        /// Perform an intersection.
+        /// </summary>
+        /// <param name="other">The area that is being intersected with this one.</param>
+        /// <returns>The intersection (=same sheet and has non-empty intersection) or null if intersection isn't possible.</returns>
+        public XLSheetArea? Intersect(XLSheetArea other)
+        {
+            if (!XLHelper.SheetComparer.Equals(Name, other.Name))
+                return null;
+
+            var intersectionRange = Area.Intersect(other.Area);
+            if (intersectionRange is null)
+                return null;
+
+            return new XLSheetArea(Name, intersectionRange.Value);
         }
     }
 }
