fc01a36bb3009d03c14c6d5a2e96546e5312499c
jahav <havlicek.honza@gmail.com>
2023-08-25 17:27:33 +0200

    Add immutable XLName to represent a name reference in formulas and other places.

---
+++ ClosedXML.Tests/Excel/Coordinates/XLNameTests.cs
@@ -0,0 +1,34 @@
+﻿using ClosedXML.Excel;
+using NUnit.Framework;
+
+namespace ClosedXML.Tests.Excel.Coordinates
+{
+    [TestFixture]
+    public class XLNameTests
+    {
+        [Test]
+        public void Workbook_name_is_compared_case_insensitive()
+        {
+            var left = new XLName("name");
+            var right = new XLName("NAME");
+
+            Assert.AreEqual(left, right);
+            Assert.AreEqual(left.GetHashCode(), right.GetHashCode());
+
+            Assert.AreNotEqual(left, new XLName("different_name"));
+        }
+
+        [Test]
+        public void Sheet_name_is_compared_case_insensitive()
+        {
+            var left = new XLName("sheet", "name");
+            var right = new XLName("SHEET", "NAME");
+
+            Assert.AreEqual(left, right);
+            Assert.AreEqual(left.GetHashCode(), right.GetHashCode());
+
+            Assert.AreNotEqual(left, new XLName("Different sheet", "name"));
+            Assert.AreNotEqual(left, new XLName("sheet", "different_name"));
+        }
+    }
+}
---
+++ ClosedXML/Excel/Coordinates/XLName.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace ClosedXML.Excel
+{
+    /// <summary>
+    /// A name in a worksheet. Unlike <see cref="IXLNamedRange"/>, this is basically only a reference.
+    /// The actual 
+    /// </summary>
+    internal readonly struct XLName : IEquatable<XLName>
+    {
+        /// <summary>
+        /// Name of a sheet. If null, the scope is a workbook. The sheet might not exist, e.g. it
+        /// is only in a formula. The name of a sheet is not escaped.
+        /// </summary>
+        public string? SheetName { get; }
+
+        /// <summary>
+        /// The defined name in the scope. Case insensitive during comparisons.
+        /// </summary>
+        public string Name { get; }
+
+        public XLName(string sheetName, string name)
+        {
+            if (name.Any(char.IsWhiteSpace))
+                throw new ArgumentException("Name can't contain whitespace.");
+
+            SheetName = sheetName;
+            Name = name;
+        }
+
+        public XLName(string name)
+        {
+            if (name.Any(char.IsWhiteSpace))
+                throw new ArgumentException("Name can't contain whitespace.");
+
+            SheetName = null;
+            Name = name;
+        }
+
+        public bool Equals(XLName other)
+        {
+            if (SheetName is null ^ other.SheetName is null)
+                return false;
+
+            if (SheetName is null && other.SheetName is null)
+                return XLHelper.NameComparer.Equals(Name, other.Name);
+
+            return XLHelper.NameComparer.Equals(Name, other.Name) &&
+                   XLHelper.SheetComparer.Equals(SheetName, other.SheetName);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is XLName other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (SheetName is not null ? XLHelper.SheetComparer.GetHashCode(SheetName) : 0) * 397;
+                hashCode ^= XLHelper.NameComparer.GetHashCode(Name);
+                return hashCode;
+            }
+        }
+    }
+}
