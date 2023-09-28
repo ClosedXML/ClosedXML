83901ea0fef32aa3fa88ad8f0c32c0bda3179e51
jahav <havlicek.honza@gmail.com>
2023-09-28 19:56:38 +0200

    Move logic to write custom file properties part to CustomFilePropertiesPartWriter. It decreases the clutter and hopefully increases encapsulation, so one writer can optimize how to write its own part without another 4k+ LOC.

---
+++ ClosedXML/Excel/IO/CustomFilePropertiesPartWriter.cs
@@ -0,0 +1,59 @@
+﻿using DocumentFormat.OpenXml.CustomProperties;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.VariantTypes;
+using System;
+
+namespace ClosedXML.Excel.IO
+{
+    internal class CustomFilePropertiesPartWriter
+    {
+        internal static void GenerateContent(CustomFilePropertiesPart customFilePropertiesPart, XLWorkbook workbook)
+        {
+            var properties = new Properties();
+            properties.AddNamespaceDeclaration("vt",
+                "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes");
+            var propertyId = 1;
+            foreach (var p in workbook.CustomProperties)
+            {
+                propertyId++;
+                var customDocumentProperty = new CustomDocumentProperty
+                {
+                    FormatId = "{D5CDD505-2E9C-101B-9397-08002B2CF9AE}",
+                    PropertyId = propertyId,
+                    Name = p.Name
+                };
+                if (p.Type == XLCustomPropertyType.Text)
+                {
+                    var vTlpwstr1 = new VTLPWSTR { Text = p.GetValue<string>() };
+                    customDocumentProperty.AppendChild(vTlpwstr1);
+                }
+                else if (p.Type == XLCustomPropertyType.Date)
+                {
+                    var vTFileTime1 = new VTFileTime
+                    {
+                        Text =
+                            p.GetValue<DateTime>().ToUniversalTime().ToString(
+                                "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'")
+                    };
+                    customDocumentProperty.AppendChild(vTFileTime1);
+                }
+                else if (p.Type == XLCustomPropertyType.Number)
+                {
+                    var vTDouble1 = new VTDouble
+                    {
+                        Text = p.GetValue<Double>().ToInvariantString()
+                    };
+                    customDocumentProperty.AppendChild(vTDouble1);
+                }
+                else
+                {
+                    var vTBool1 = new VTBool { Text = p.GetValue<Boolean>().ToString().ToLower() };
+                    customDocumentProperty.AppendChild(vTBool1);
+                }
+                properties.AppendChild(customDocumentProperty);
+            }
+
+            customFilePropertiesPart.Properties = properties;
+        }
+    }
+}
--- ClosedXML/Excel/XLWorkbook_Save.cs
+++ ClosedXML/Excel/XLWorkbook_Save.cs
@@ -356,7 +356,7 @@ namespace ClosedXML.Excel
                 var customFilePropertiesPart =
                     document.CustomFilePropertiesPart ?? document.AddNewPart<CustomFilePropertiesPart>(context.RelIdGenerator.GetNext(RelType.Workbook));
 
-                GenerateCustomFilePropertiesPartContent(customFilePropertiesPart);
+                CustomFilePropertiesPartWriter.GenerateContent(customFilePropertiesPart, this);
             }
             else
             {
@@ -1557,56 +1557,7 @@ namespace ClosedXML.Excel
 
             themePart.Theme = theme1;
         }
-
-        private void GenerateCustomFilePropertiesPartContent(CustomFilePropertiesPart customFilePropertiesPart)
-        {
-            var properties = new DocumentFormat.OpenXml.CustomProperties.Properties();
-            properties.AddNamespaceDeclaration("vt",
-                "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes");
-            var propertyId = 1;
-            foreach (var p in CustomProperties)
-            {
-                propertyId++;
-                var customDocumentProperty = new CustomDocumentProperty
-                {
-                    FormatId = "{D5CDD505-2E9C-101B-9397-08002B2CF9AE}",
-                    PropertyId = propertyId,
-                    Name = p.Name
-                };
-                if (p.Type == XLCustomPropertyType.Text)
-                {
-                    var vTlpwstr1 = new VTLPWSTR { Text = p.GetValue<string>() };
-                    customDocumentProperty.AppendChild(vTlpwstr1);
-                }
-                else if (p.Type == XLCustomPropertyType.Date)
-                {
-                    var vTFileTime1 = new VTFileTime
-                    {
-                        Text =
-                            p.GetValue<DateTime>().ToUniversalTime().ToString(
-                                "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'")
-                    };
-                    customDocumentProperty.AppendChild(vTFileTime1);
-                }
-                else if (p.Type == XLCustomPropertyType.Number)
-                {
-                    var vTDouble1 = new VTDouble
-                    {
-                        Text = p.GetValue<Double>().ToInvariantString()
-                    };
-                    customDocumentProperty.AppendChild(vTDouble1);
-                }
-                else
-                {
-                    var vTBool1 = new VTBool { Text = p.GetValue<Boolean>().ToString().ToLower() };
-                    customDocumentProperty.AppendChild(vTBool1);
-                }
-                properties.AppendChild(customDocumentProperty);
-            }
-
-            customFilePropertiesPart.Properties = properties;
-        }
-
+        
         private void SetPackageProperties(OpenXmlPackage document)
         {
             var created = Properties.Created == DateTime.MinValue ? DateTime.Now : Properties.Created;
