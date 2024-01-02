a9d18616c20fe6701cc0098e941cf4b548dc9e89
jahav <havlicek.honza@gmail.com>
2024-01-02 20:28:29 +0100

    Add tests for dynamic filters.

--- ClosedXML.Tests/Excel/AutoFilters/DynamicFilterTests.cs
+++ ClosedXML.Tests/Excel/AutoFilters/DynamicFilterTests.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using System.Linq;
+using ClosedXML.Excel;
 
 namespace ClosedXML.Tests.Excel.AutoFilters
 {
@@ -25,6 +27,44 @@ namespace ClosedXML.Tests.Excel.AutoFilters
                     var filterResult = ws.Rows("2:7").Select(row => !row.IsHidden);
                     CollectionAssert.AreEqual(new[] { false, false, false, false, true, true }, filterResult);
                 });
+        }
+
+        [Test]
+        public void BelowAverage_takes_values_under_avg_value()
+        {
+            // The average 2 is not included. 
+            new AutoFilterTester(f => f.BelowAverage())
+                .AddTrue(1)
+                .AddFalse(2, 3)
+                .AssertVisibility();
+        }
+
+        [Test]
+        public void AboveAverage_takes_values_over_avg_value()
+        {
+            new AutoFilterTester(f => f.AboveAverage())
+                .AddTrue(3)
+                .AddFalse(2, 1)
+                .AssertVisibility();
+        }
+
+        [Test]
+        public void Average_ignores_non_unified_numbers()
+        {
+            new AutoFilterTester(f => f.BelowAverage())
+                .AddTrue(new DateTime(1900, 1, 1)) // Serial date time 1
+                .AddFalse(1.1)
+                .AddFalse(1.2)
+                .AddFalse(XLError.NoValueAvailable, true, false, "-100", "Text", Blank.Value)
+                .AssertVisibility();
+        }
+
+        [Test]
+        public void All_rows_are_hidden_when_column_has_no_number()
+        {
+            new AutoFilterTester(f => f.AboveAverage())
+                .AddFalse(Blank.Value, true, false, "-100", "Text", XLError.NoValueAvailable)
+                .AssertVisibility();
         }
     }
 }
