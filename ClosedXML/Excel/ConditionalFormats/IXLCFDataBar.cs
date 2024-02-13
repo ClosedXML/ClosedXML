78f425ff50f4a201e36477858f17853ecd575b89
erenes <eer@eer.cc>
2024-02-13 21:55:49 +0100

    Add documentation

--- ClosedXML/Excel/ConditionalFormats/IXLCFDataBar.cs
+++ ClosedXML/Excel/ConditionalFormats/IXLCFDataBar.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿#nullable disable
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ClosedXML.Excel
 {
+    /// <summary>
+    /// A fluent configuration builder for the DataBar object
+    /// </summary>
     public interface IXLCFDataBar
     {
+        /// <summary>
+        /// The fill type of a DataBar, Gradient (true) or Solid.
+        /// </summary>
         bool Gradient { get; }
 
+        /// <summary>
+        /// Specifies the fill type of a DataBar, Gradient or Solid.
+        /// </summary>
+        /// <param name="value">true to apply a gradient fill (default), false to apply a solid fill</param>
+        /// <returns>The <see cref="IXLCFDataBar" /></returns>
         IXLCFDataBar SetGradient(bool value = true);
     }
 }
--- ClosedXML/Excel/ConditionalFormats/XLCFDataBar.cs
+++ ClosedXML/Excel/ConditionalFormats/XLCFDataBar.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable disable
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -16,8 +18,16 @@ namespace ClosedXML.Excel
             Gradient = true;
         }
 
+        /// <summary>
+        /// The fill type of a DataBar, Gradient (true) or Solid.
+        /// </summary>
         public bool Gradient { get; set; }
 
+        /// <summary>
+        /// Specifies the fill type of a DataBar, Gradient or Solid.
+        /// </summary>
+        /// <param name="value">true to apply a gradient fill (default), false to apply a solid fill</param>
+        /// <returns>The <see cref="IXLCFDataBar" /></returns>
         public IXLCFDataBar SetGradient(bool value = true)
         {
             Gradient = value;
