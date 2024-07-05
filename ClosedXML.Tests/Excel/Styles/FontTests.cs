7fe6253aa2be56f013786d4499f0d469445d6955
jahav <havlicek.honza@gmail.com>
2024-07-05 14:32:04 +0200

    Mark all properties of XLFontKey as required.
    If we ever add another property, all instantiation places should
    report an error. It's rather easy to forget one (e.g. shadow in
    default key)

--- ClosedXML.Tests/Excel/Styles/FontTests.cs
+++ ClosedXML.Tests/Excel/Styles/FontTests.cs
@@ -5,12 +5,14 @@ namespace ClosedXML.Tests.Excel.Styles
 {
     public class FontTests
     {
+        private readonly XLFontKey _defaultKey = XLFontValue.Default.Key;
+
         [Test]
         public void XLFontKey_GetHashCode_IsCaseInsensitive()
         {
-            var fontKey1 = new XLFontKey { FontName = "Arial" };
-            var fontKey2 = new XLFontKey { FontName = "Times New Roman" };
-            var fontKey3 = new XLFontKey { FontName = "TIMES NEW ROMAN" };
+            var fontKey1 = _defaultKey with { FontName = "Arial" };
+            var fontKey2 = _defaultKey with { FontName = "Times New Roman" };
+            var fontKey3 = _defaultKey with { FontName = "TIMES NEW ROMAN" };
 
             Assert.AreNotEqual(fontKey1.GetHashCode(), fontKey2.GetHashCode());
             Assert.AreEqual(fontKey2.GetHashCode(), fontKey3.GetHashCode());
@@ -19,9 +21,9 @@ namespace ClosedXML.Tests.Excel.Styles
         [Test]
         public void XLFontKey_Equals_IsCaseInsensitive()
         {
-            var fontKey1 = new XLFontKey { FontName = "Arial" };
-            var fontKey2 = new XLFontKey { FontName = "Times New Roman" };
-            var fontKey3 = new XLFontKey { FontName = "TIMES NEW ROMAN" };
+            var fontKey1 = _defaultKey with { FontName = "Arial" };
+            var fontKey2 = _defaultKey with { FontName = "Times New Roman" };
+            var fontKey3 = _defaultKey with { FontName = "TIMES NEW ROMAN" };
 
             Assert.IsFalse(fontKey1.Equals(fontKey2));
             Assert.IsTrue(fontKey2.Equals(fontKey3));
