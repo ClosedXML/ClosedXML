29798107abf198bf690c35ffa01b0fb8d3f17599
jahav <havlicek.honza@gmail.com>
2024-07-28 22:03:38 +0200

    Add xml docs for pivot style function.

--- ClosedXML/Excel/PivotTables/PivotStyleFormats/IXLPivotFieldStyleFormats.cs
+++ ClosedXML/Excel/PivotTables/PivotStyleFormats/IXLPivotFieldStyleFormats.cs
@@ -13,8 +13,16 @@ namespace ClosedXML.Excel
         /// </summary>
         IXLPivotValueStyleFormat DataValuesFormat { get; }
 
+    /// <summary>
+    /// Get the style of the pivot field header. The head usually contains a name of the field.
+    /// In some layouts, header is not individually displayed (e.g. compact), while in others
+    /// it is (e.g. tabular).
+    /// </summary>
         IXLPivotStyleFormat Header { get; }
 
+    /// <summary>
+    /// Get the style of the pivot field label values on horizontal or vertical axis.
+    /// </summary>
         IXLPivotStyleFormat Label { get; }
 
         IXLPivotStyleFormat Subtotal { get; }
