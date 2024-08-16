dc2615d52f577cc3b3a42d7cf751e821eb5b6599
Francois Botha <igitur@gmail.com>
2024-08-16 11:49:40 +0200

    Correctly load quoted and unquoted sheet names in print area definitions

---
+++ ClosedXML.Tests/Excel/PageSetup/PrintAreaTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace ClosedXML.Tests.Excel
+{
+    [TestFixture]
+    public class PrintAreaTests
+    {
+        [Test]
+        [TestCase("A1:B2")]
+        [TestCase("A1:B2", "D3:D5")]
+        public void CanLoadWorksheetWithMultiplePrintAreas(params string[] printAreaRangeAddresses)
+        {
+            TestHelper.CreateSaveLoadAssert(
+                (_, ws) =>
+                {
+                    foreach (var printAreaRangeAddress in printAreaRangeAddresses)
+                        ws.PageSetup.PrintAreas.Add(printAreaRangeAddress);
+                },
+                (_, ws) =>
+                {
+                    var actualPrintAddresses = ws.PageSetup.PrintAreas.Select(pa => pa.RangeAddress.ToStringRelative());
+                    CollectionAssert.AreEqual(printAreaRangeAddresses, actualPrintAddresses);
+                });
+        }
+    }
+}
