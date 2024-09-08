b2bf8263f355c249a20fa8d022c59169bf9ec0e1
jahav <havlicek.honza@gmail.com>
2024-09-09 01:49:12 +0200

    Can style pivot value and axis field intersection
    This once again returns previously disabled features that were removed
    due to strorage structure switch.
    
    The value field can't be referenced through field reference, because
    there can be multiple value fields for a single cache field. The
    pivot table structure therefore uses 'data' field (aka -2) and stored
    index of value field as a field item.

--- ClosedXML/Excel/PivotTables/PivotStyleFormats/XLPivotValueStyleFormat.cs
+++ ClosedXML/Excel/PivotTables/PivotStyleFormats/XLPivotValueStyleFormat.cs
@@ -12,19 +12,19 @@ internal class XLPivotValueStyleFormat : XLPivotStyleFormatBase, IXLPivotValueSt
     /// The term "lie on" means that either column or a row of data cell
     /// intersects a label cell of referenced field.
     /// </summary>
-    private readonly List<FieldIndex> _fieldReferences = new();
+    private readonly List<FieldReference> _fieldReferences = new();
 
     public XLPivotValueStyleFormat(XLPivotTable pivotTable, FieldIndex fieldIndex)
         : base(pivotTable)
     {
-        _fieldReferences.Add(fieldIndex);
+        _fieldReferences.Add(new FieldReference(fieldIndex));
     }
 
     #region IXLPivotValueStyleFormat members
 
     public IXLPivotValueStyleFormat AndWith(IXLPivotField field)
     {
-        _fieldReferences.Add(field.Offset);
+        _fieldReferences.Add(new FieldReference(field.Offset));
         return this;
     }
 
@@ -35,7 +35,12 @@ internal class XLPivotValueStyleFormat : XLPivotStyleFormatBase, IXLPivotValueSt
 
     public IXLPivotValueStyleFormat ForValueField(IXLPivotValue valueField)
     {
-        throw new NotImplementedException();
+        var valuesIndex = PivotTable.DataFields.IndexOf(valueField);
+        if (valuesIndex == -1)
+            throw new ArgumentOutOfRangeException($"Field '{valueField.CustomName}' is not among value fields of the pivot table.");
+
+        _fieldReferences.Add(new FieldReference(FieldIndex.DataField, new[] { (uint)valuesIndex }));
+        return this;
     }
 
     #endregion IXLPivotValueStyleFormat members
@@ -44,7 +49,19 @@ internal class XLPivotValueStyleFormat : XLPivotStyleFormatBase, IXLPivotValueSt
     {
         var area = new XLPivotArea();
         foreach (var fieldReference in _fieldReferences)
-            area.AddReference(new XLPivotReference() { Field = unchecked((uint?)fieldReference.Value) });
+        {
+            var reference = new XLPivotReference
+            {
+                Field = unchecked((uint?)fieldReference.FieldIndex.Value)
+            };
+            if (fieldReference.Items is not null)
+            {
+                foreach (var item in fieldReference.Items)
+                    reference.AddFieldItem(item);
+            }
+
+            area.AddReference(reference);
+        }
 
         return area;
     }
@@ -54,4 +71,6 @@ internal class XLPivotValueStyleFormat : XLPivotStyleFormatBase, IXLPivotValueSt
         var currentArea = GetCurrentArea();
         return XLPivotAreaComparer.Instance.Equals(area, currentArea);
     }
+
+    private record FieldReference(FieldIndex FieldIndex, uint[]? Items = null);
 }
