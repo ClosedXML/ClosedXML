60faaaff80cb96ee644fe9d23fae2f6e11ba4bf5
jahav <havlicek.honza@gmail.com>
2024-12-23 20:19:23 +0100

    Add CommonCrawl program

--- ClosedXML.Sandbox/Program.cs
+++ ClosedXML.Sandbox/Program.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Xml;
-using ClosedXML.Excel;
 using ClosedXML.Excel.IO;
 
 namespace ClosedXML.Sandbox
@@ -10,12 +12,87 @@ namespace ClosedXML.Sandbox
     {
         private static void Main(string[] args)
         {
-            using var stream = File.Open(@"d:\temp\styles.xml", FileMode.Open, FileAccess.Read);
-            using var xmlReader = XmlReader.Create(stream);
-            xmlReader.MoveToContent();
-            var reader = new XmlTreeReader(xmlReader);
+            long len = 0;
+            var sw = new Stopwatch();
+            var commonCrawlDir = @"d:\temp\cc";
+            var files = Directory.EnumerateFiles(commonCrawlDir, "*.zip").ToList();//.Where(x => x.Contains("136c58acbbc5fc46031f2d7d370fb6934d44c28019ef8fd7aebd9c0817dc81c9"));
+            var count = 0;
+            var errorCount = 0;
+            var zipErrorCount = 0;
+            var styleLessCount = 0;
+            foreach (var filePath in files.Take(100000))
+            {
+                count++;
+                if (count % 100 == 0)
+                    Console.Write('.');
 
-            new StyleSheetReader().Load(reader);
+                if (count % 5000 == 0)
+                {
+                    Console.WriteLine("\n{0:N} ms", sw.ElapsedMilliseconds);
+                    Console.WriteLine("Total len {0:N}", len);
+                }
+
+                using var f = File.OpenRead(filePath);
+                ZipArchive archive;
+                try
+                {
+                    archive = new ZipArchive(f, ZipArchiveMode.Read);
+                }
+                catch
+                {
+                    zipErrorCount++;
+                    continue;
+                }
+                
+                ZipArchiveEntry styles;
+                try
+                {
+                    styles = archive.GetEntry("xl/styles.xml");
+                    if (styles is null)
+                    {
+                        styleLessCount++;
+                        continue;
+                    }
+                }
+                catch
+                {
+                    zipErrorCount++;
+                    continue;
+                }
+
+                using var styleStream = styles.Open();
+                len += styles.Length;
+                try
+                {
+                    using var xmlReader = XmlReader.Create(styleStream);
+                    xmlReader.MoveToContent();
+                    var reader = new XmlTreeReader(xmlReader);
+
+                    sw.Start();
+                    new StyleSheetReader().Load(reader);
+                }
+                catch (Exception ex)
+                {
+                    errorCount++;
+                    Console.WriteLine($"\n{Path.GetFileName(filePath)} ERROR: " + ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+                finally
+                {
+                    sw.Stop();
+                }
+
+                archive.Dispose();
+                
+            }
+
+
+            Console.WriteLine("Total: {0}", count);
+            Console.WriteLine("Errors: {0}", errorCount);
+            Console.WriteLine("ZipErrors: {0}", zipErrorCount);
+            Console.WriteLine("StyleLessCount: {0}", styleLessCount);
+            Console.WriteLine("{0:N} ms", sw.ElapsedMilliseconds);
+            Console.WriteLine("Total len {0:N}", len);
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
         }
