075d11aeda9467bd7aa2356f98552c28d6eaaa68
jahav <havlicek.honza@gmail.com>
2025-03-26 23:17:58 +0100

    Move code to read sheetViews to sheet part reader

--- ClosedXML/Excel/IO/WorksheetPartReader.cs
+++ ClosedXML/Excel/IO/WorksheetPartReader.cs
@@ -43,4 +43,55 @@ internal static class WorksheetPartReader
         if (sheetProperty.PageSetupProperties != null)
             pageSetupProperties = sheetProperty.PageSetupProperties;
     }
+
+    public static void LoadSheetViews(SheetViews sheetViews, XLWorksheet ws)
+    {
+        if (sheetViews == null) return;
+
+        var sheetView = sheetViews.Elements<SheetView>().FirstOrDefault();
+
+        if (sheetView == null) return;
+
+        if (sheetView.RightToLeft != null) ws.RightToLeft = sheetView.RightToLeft.Value;
+        if (sheetView.ShowFormulas != null) ws.ShowFormulas = sheetView.ShowFormulas.Value;
+        if (sheetView.ShowGridLines != null) ws.ShowGridLines = sheetView.ShowGridLines.Value;
+        if (sheetView.ShowOutlineSymbols != null)
+            ws.ShowOutlineSymbols = sheetView.ShowOutlineSymbols.Value;
+        if (sheetView.ShowRowColHeaders != null) ws.ShowRowColHeaders = sheetView.ShowRowColHeaders.Value;
+        if (sheetView.ShowRuler != null) ws.ShowRuler = sheetView.ShowRuler.Value;
+        if (sheetView.ShowWhiteSpace != null) ws.ShowWhiteSpace = sheetView.ShowWhiteSpace.Value;
+        if (sheetView.ShowZeros != null) ws.ShowZeros = sheetView.ShowZeros.Value;
+        if (sheetView.TabSelected != null) ws.TabSelected = sheetView.TabSelected.Value;
+
+        var selection = sheetView.Elements<Selection>().FirstOrDefault();
+        if (selection != null)
+        {
+            if (selection.SequenceOfReferences != null)
+                ws.Ranges(selection.SequenceOfReferences.InnerText.Replace(" ", ",")).Select();
+
+            if (selection.ActiveCell != null)
+                ws.Cell(selection.ActiveCell).SetActive();
+        }
+
+        if (sheetView.ZoomScale != null)
+            ws.SheetView.ZoomScale = (int)UInt32Value.ToUInt32(sheetView.ZoomScale);
+        if (sheetView.ZoomScaleNormal != null)
+            ws.SheetView.ZoomScaleNormal = (int)UInt32Value.ToUInt32(sheetView.ZoomScaleNormal);
+        if (sheetView.ZoomScalePageLayoutView != null)
+            ws.SheetView.ZoomScalePageLayoutView = (int)UInt32Value.ToUInt32(sheetView.ZoomScalePageLayoutView);
+        if (sheetView.ZoomScaleSheetLayoutView != null)
+            ws.SheetView.ZoomScaleSheetLayoutView = (int)UInt32Value.ToUInt32(sheetView.ZoomScaleSheetLayoutView);
+
+        var pane = sheetView.Elements<Pane>().FirstOrDefault();
+        if (new[] { PaneStateValues.Frozen, PaneStateValues.FrozenSplit }.Contains(pane?.State?.Value ?? PaneStateValues.Split))
+        {
+            if (pane.HorizontalSplit != null)
+                ws.SheetView.SplitColumn = (Int32)pane.HorizontalSplit.Value;
+            if (pane.VerticalSplit != null)
+                ws.SheetView.SplitRow = (Int32)pane.VerticalSplit.Value;
+        }
+
+        if (XLHelper.IsValidA1Address(sheetView.TopLeftCell))
+            ws.SheetView.TopLeftCellAddress = ws.Cell(sheetView.TopLeftCell.Value).Address;
+    }
 }
--- ClosedXML/Excel/XLWorkbook_Load.cs
+++ ClosedXML/Excel/XLWorkbook_Load.cs
@@ -311,7 +311,7 @@ namespace ClosedXML.Excel
                             }
                         }
                         else if (reader.ElementType == typeof(SheetViews))
-                            LoadSheetViews((SheetViews)reader.LoadCurrentElement(), ws);
+                            WorksheetPartReader.LoadSheetViews((SheetViews)reader.LoadCurrentElement(), ws);
                         else if (reader.ElementType == typeof(MergeCells))
                         {
                             var mergedCells = (MergeCells)reader.LoadCurrentElement();
@@ -2467,57 +2467,6 @@ namespace ClosedXML.Excel
                 ws.PageSetup.CenterVertically = printOptions.VerticalCentered;
             if (printOptions.Headings != null)
                 ws.PageSetup.ShowRowAndColumnHeadings = printOptions.Headings;
-        }
-
-        private static void LoadSheetViews(SheetViews sheetViews, XLWorksheet ws)
-        {
-            if (sheetViews == null) return;
-
-            var sheetView = sheetViews.Elements<SheetView>().FirstOrDefault();
-
-            if (sheetView == null) return;
-
-            if (sheetView.RightToLeft != null) ws.RightToLeft = sheetView.RightToLeft.Value;
-            if (sheetView.ShowFormulas != null) ws.ShowFormulas = sheetView.ShowFormulas.Value;
-            if (sheetView.ShowGridLines != null) ws.ShowGridLines = sheetView.ShowGridLines.Value;
-            if (sheetView.ShowOutlineSymbols != null)
-                ws.ShowOutlineSymbols = sheetView.ShowOutlineSymbols.Value;
-            if (sheetView.ShowRowColHeaders != null) ws.ShowRowColHeaders = sheetView.ShowRowColHeaders.Value;
-            if (sheetView.ShowRuler != null) ws.ShowRuler = sheetView.ShowRuler.Value;
-            if (sheetView.ShowWhiteSpace != null) ws.ShowWhiteSpace = sheetView.ShowWhiteSpace.Value;
-            if (sheetView.ShowZeros != null) ws.ShowZeros = sheetView.ShowZeros.Value;
-            if (sheetView.TabSelected != null) ws.TabSelected = sheetView.TabSelected.Value;
-
-            var selection = sheetView.Elements<Selection>().FirstOrDefault();
-            if (selection != null)
-            {
-                if (selection.SequenceOfReferences != null)
-                    ws.Ranges(selection.SequenceOfReferences.InnerText.Replace(" ", ",")).Select();
-
-                if (selection.ActiveCell != null)
-                    ws.Cell(selection.ActiveCell).SetActive();
-            }
-
-            if (sheetView.ZoomScale != null)
-                ws.SheetView.ZoomScale = (int)UInt32Value.ToUInt32(sheetView.ZoomScale);
-            if (sheetView.ZoomScaleNormal != null)
-                ws.SheetView.ZoomScaleNormal = (int)UInt32Value.ToUInt32(sheetView.ZoomScaleNormal);
-            if (sheetView.ZoomScalePageLayoutView != null)
-                ws.SheetView.ZoomScalePageLayoutView = (int)UInt32Value.ToUInt32(sheetView.ZoomScalePageLayoutView);
-            if (sheetView.ZoomScaleSheetLayoutView != null)
-                ws.SheetView.ZoomScaleSheetLayoutView = (int)UInt32Value.ToUInt32(sheetView.ZoomScaleSheetLayoutView);
-
-            var pane = sheetView.Elements<Pane>().FirstOrDefault();
-            if (new[] { PaneStateValues.Frozen, PaneStateValues.FrozenSplit }.Contains(pane?.State?.Value ?? PaneStateValues.Split))
-            {
-                if (pane.HorizontalSplit != null)
-                    ws.SheetView.SplitColumn = (Int32)pane.HorizontalSplit.Value;
-                if (pane.VerticalSplit != null)
-                    ws.SheetView.SplitRow = (Int32)pane.VerticalSplit.Value;
-            }
-
-            if (XLHelper.IsValidA1Address(sheetView.TopLeftCell))
-                ws.SheetView.TopLeftCellAddress = ws.Cell(sheetView.TopLeftCell.Value).Address;
         }
 
         private void SetProperties(SpreadsheetDocument dSpreadsheet)
