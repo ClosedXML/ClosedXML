a4a07d34b6a47e53ca835aedc2b8e8ca8280f673
jahav <havlicek.honza@gmail.com>
2025-03-27 23:37:26 +0100

    Refactor VML color parsing so it doesn't throw (#2627)
    VML parsing used to throw on strings that started with # and weren't RGB colors, e.g. #NFOBACKGROUND. The whole parsing shouldn't throw on basically anything.
    
    The VmlNamedColors use symbolic names, because that way we can preserve name for roundtrip saving.

---
+++ ClosedXML.Tests/Excel/Styles/XLColorTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using ClosedXML.Excel;
+using NUnit.Framework;
+
+namespace ClosedXML.Tests.Excel;
+
+public class XLColorTests
+{
+    public static IEnumerable<object[]> VmlColors
+    {
+        get
+        {
+            // Hexadecimal color
+            yield return new object[] { "#F0E0D0", Color.FromArgb(0xF0, 0xE0, 0xD0) };
+
+            // Named color
+            yield return new object[] { "red", Color.Red };
+
+            // Palette color
+            yield return new object[] { "Menu [30]", Color.FromArgb(0xF0, 0xF0, 0xF0) };
+            yield return new object[] { "Menu", Color.FromArgb(0xF0, 0xF0, 0xF0) };
+
+            // Unknown/malformed color
+            yield return new object[] { "#NFOBACKGROUND", Color.FromName("#NFOBACKGROUND") };
+        }
+    }
+
+    [TestCaseSource(nameof(VmlColors))]
+    public void FromVmlColor_converts_hexadecimal_colors(string colorText, Color expectedColor)
+    {
+        var color = XLColor.FromVmlColor(colorText);
+
+        Assert.That(color, Is.EqualTo(XLColor.FromColor(expectedColor)));
+    }
+}
