80a0cdf6b04e165bd30f7602ab660fd8376fe7e8
jahav <havlicek.honza@gmail.com>
2025-04-12 22:15:26 +0200

    Add decoder for XString into IO

---
+++ ClosedXML.IO/XStringConvert.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace ClosedXML.IO;
+
+/// <summary>
+/// Class to deal with encoding and decoding XString. XString decoding doesn't depend on the source
+/// encoding. XString decoding decodes from unicode codepoints (regardless if UTF-8 or UTF-16)
+/// and the decoder replaces XString patterns with decoded codepoints.
+/// </summary>
+public class XStringConvert
+{
+    /// <summary>
+    /// Decode an XString to normal string.
+    /// </summary>
+    /// <param name="text">Test that might contain XString encoded characters.</param>
+    /// <returns>Decoded string.</returns>
+    public static string Decode(string text)
+    {
+        // This method is on a hotpath and shouldn't allocate unless necessary.
+        // Do lazy initialization, there might not be any pattern at all.
+        StringBuilder? sb = null;
+
+        // An index of next character after last encountered XString pattern.
+        // Initial value is 0 because text from that index on is copied.
+        var prevSliceNextCharIndex = 0;
+        var textSpan = text.AsSpan();
+        for (var i = textSpan.IndexOf('_'); i >= 0 && i < text.Length - 6; i = text.IndexOf('_', i + 1))
+        {
+            if (IsPattern(textSpan, i))
+            {
+                sb ??= new StringBuilder(text.Length);
+
+                // Append text from last XString splice
+                sb.Append(text, prevSliceNextCharIndex, i - prevSliceNextCharIndex);
+
+                // Get hex digits from from _xABCD_ patterns. Polyfill doesn't have allocation-free
+                // API, so just decode the hex number.
+                var codepoint = 0;
+                for (var hexIndex = i + 2; hexIndex < i + 6; ++hexIndex)
+                {
+                    var hexDigit = GetHex(textSpan[hexIndex]);
+                    codepoint = (codepoint * 16) + hexDigit;
+                }
+
+                sb.Append((char)codepoint);
+
+                // Move from opening '_' to closing '_' because we have effectively read all that
+                // The loop will add + 1 and moves to the next char of text.
+                i += 6;
+                prevSliceNextCharIndex = i + 1;
+            }
+        }
+
+        // Not even one pattern was actually replaced -> return original text
+        if (sb is null)
+            return text;
+
+        sb.Append(text, prevSliceNextCharIndex, text.Length - prevSliceNextCharIndex);
+        return sb.ToString();
+
+        // Does the XString pattern starts at index i?
+        static bool IsPattern(ReadOnlySpan<char> input, int i)
+        {
+            // Reorder to ensure simplest tests are checked first
+            return i + 6 < input.Length &&
+                   input[i] == '_' &&
+                   input[i + 1] == 'x' &&
+                   input[i + 6] == '_' &&
+                   IsHex(input[i + 2]) &&
+                   IsHex(input[i + 3]) &&
+                   IsHex(input[i + 4]) &&
+                   IsHex(input[i + 5]);
+        }
+
+        static bool IsHex(char c)
+        {
+            return c is >= '0' and <= '9' ||
+                   c is >= 'A' and <= 'F' ||
+                   c is >= 'a' and <= 'f';
+        }
+
+        // We already know that c passed the IsHex method.
+        static int GetHex(char c)
+        {
+            return c switch
+            {
+                >= 'A' and <= 'F' => c - 'A' + 10,
+                >= 'a' and <= 'f' => c - 'a' + 10,
+                >= '0' and <= '9' => c - '0',
+                _ => throw new UnreachableException()
+            };
+        }
+    }
+}
---
+++ ClosedXML.Tests/IO/XStringConvertTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ClosedXML.IO;
+
+namespace ClosedXML.Tests.IO;
+
+internal class XStringConvertTests
+{
+    [TestCase("", "")]
+    [TestCase("_x000D_", "\r")]
+    [TestCase("_x30ab_", "カ")] // Hexadecimal numbers are case insensitive
+    [TestCase("_x0009_", "\t")]
+    [TestCase("__x0041__", "_A_")]
+    [TestCase("A_x0042_C", "ABC")]
+    [TestCase("_X0041_", "_X0041_")] // Must be lowercase x in the pattern
+    [TestCase("_x263A_", "\u263a")] // Smiley face
+    [TestCase("_xD83D__xDE43_", "\ud83d\ude43")] // Astral planes - Upside down smiley face
+    [TestCase("Result:_x0009_ _x0057_", "Result:\t W")]
+    [TestCase("DE_x005F_xAB50_0161_title", "DE_xAB50_0161_title")]
+    public void Decodes_encoded_unicode_characters(string sourceText, string expectedText)
+    {
+        var decodedText = XStringConvert.Decode(sourceText);
+
+        Assert.That(decodedText, Is.EqualTo(expectedText));
+    }
+}
