ce55afeffdc446af0525a5c5d417d7f958895c6e
jahav <havlicek.honza@gmail.com>
2025-05-02 22:22:56 +0200

    Remove unused methods

--- ClosedXML/Utils/CryptographicAlgorithms.cs
+++ ClosedXML/Utils/CryptographicAlgorithms.cs
@@ -11,18 +11,6 @@ namespace ClosedXML.Utils
 {
     internal static class CryptographicAlgorithms
     {
-        public static string Base64Decode(string base64EncodedData)
-        {
-            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
-            return Encoding.UTF8.GetString(base64EncodedBytes);
-        }
-
-        public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            return Convert.ToBase64String(plainTextBytes);
-        }
-
         public static String GenerateNewSalt(Algorithm algorithm)
         {
             if (RequiresSalt(algorithm))
