6075a15bcbc8fe2cfad4f713495186b3216d2e3e
jahav <havlicek.honza@gmail.com>
2025-05-02 22:09:38 +0200

    Remove unused methods

--- ClosedXML/Excel/CalcEngine/Functions/XLMath.cs
+++ ClosedXML/Excel/CalcEngine/Functions/XLMath.cs
@@ -4,36 +4,6 @@ namespace ClosedXML.Excel.CalcEngine.Functions
 {
     public static class XLMath
     {
-        public static double DegreesToRadians(double degrees)
-        {
-            return (Math.PI / 180.0) * degrees;
-        }
-
-        public static double RadiansToDegrees(double radians)
-        {
-            return (180.0 / Math.PI) * radians;
-        }
-
-        public static double GradsToRadians(double grads)
-        {
-            return (grads / 200.0) * Math.PI;
-        }
-
-        public static double RadiansToGrads(double radians)
-        {
-            return (radians / Math.PI) * 200.0;
-        }
-
-        public static double DegreesToGrads(double degrees)
-        {
-            return (degrees / 9.0) * 10.0;
-        }
-
-        public static double GradsToDegrees(double grads)
-        {
-            return (grads / 10.0) * 9.0;
-        }
-
         public static double Asinh(double x)
         {
             return (Math.Log(x + Math.Sqrt(x * x + 1.0)));
@@ -49,35 +19,9 @@ namespace ClosedXML.Excel.CalcEngine.Functions
             return (Math.Log((1.0 + x) / (1.0 - x)) / 2.0);
         }
 
-        public static double ACoth(double x)
-        {
-            //return (Math.Log((x + 1.0) / (x - 1.0)) / 2.0);
-            return (ATanh(1.0 / x));
-        }
-
-        public static double ASech(double x)
-        {
-            return (ACosh(1.0 / x));
-        }
-
-        public static double ACsch(double x)
-        {
-            return (Asinh(1.0 / x));
-        }
-
-        public static double Sech(double x)
-        {
-            return (1.0 / Math.Cosh(x));
-        }
-
         public static double Csch(double x)
         {
             return (1.0 / Math.Sinh(x));
-        }
-
-        public static double Coth(double x)
-        {
-            return (Math.Cosh(x) / Math.Sinh(x));
         }
 
         internal static OneOf<double, XLError> CombinChecked(double number, double numberChosen)
