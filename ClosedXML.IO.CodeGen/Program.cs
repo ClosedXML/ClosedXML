866c829cb6de1b365ef025ec57efbe618ff28453
jahav <havlicek.honza@gmail.com>
2025-05-04 15:55:05 +0200

    Move code to separate method

--- ClosedXML.IO.CodeGen/Program.cs
+++ ClosedXML.IO.CodeGen/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using ClosedXML.IO.CodeGen.Model;
 using ClosedXML.IO.CodeGen.XsdParser;
 
 namespace ClosedXML.IO.CodeGen;
@@ -36,46 +37,51 @@ public class Program
                 visitor.Visit(schema);
                 var outputFile = Path.ChangeExtension(schemaPath, "copy");
                 File.WriteAllText(outputFile, sb.ToString());
-
                 Console.WriteLine($"Wrote copy to {outputFile}");
                 break;
 
             case "cache-records":
-                var cacheRecordsGenerator = new ParserGenerator(schema, "PivotCacheRecordsReader", "_ns")
-                        .WithNamespace("ClosedXML.Excel.IO")
-                        .AddSimpleTypeRequired<uint>("xsd:unsignedInt", "_reader.GetUInt(\"{0}\")")
-                        .AddSimpleTypeOptional<int?>("xsd:int", "_reader.GetOptionalInt(\"{0}\")")
-                        .AddSimpleTypeRequired<bool>("xsd:boolean", "_reader.GetBool(\"{0}\")")
-                        .AddSimpleTypeOptional<bool?>("xsd:boolean", "_reader.GetOptionalBool(\"{0}\")")
-                        .AddSimpleTypeOptional<string?>("s:ST_Xstring", "_reader.GetOptionalXString(\"{0}\")")
-                        .AddSimpleTypeRequired<string>("s:ST_Xstring", "_reader.GetXString(\"{0}\")")
-                        .AddSimpleTypeOptional<uint?>("xsd:unsignedInt", "_reader.GetOptionalUInt(\"{0}\")")
-                        .AddSimpleTypeRequired<DateTime>("xsd:dateTime", "_reader.GetDateTime(\"{0}\")")
-                        .AddSimpleTypeOptional<uint?>("ST_UnsignedIntHex", "_reader.GetOptionalUIntHex(\"{0}\")")
-                        .AddSimpleTypeRequired<double>("xsd:double", "_reader.GetDouble(\"{0}\")")
-
-                        // CT_PivotCacheRecords - hand-coded
-                        .AddParseMethod("CT_Record")
-                        .AddParseMethod("CT_Missing")
-                        .AddParseMethod("CT_Number")
-                        .AddParseMethod("CT_Boolean")
-                        .AddParseMethod("CT_Error")
-                        .AddParseMethod("CT_String")
-                        .AddParseMethod("CT_DateTime")
-                        .AddParseMethod("CT_Index")
-                        .AddParseMethod("CT_X")
-                        .AddParseMethod("CT_Tuples")
-                        .AddParseMethod("CT_Tuple")
-                    ;
-
-                var cacheRecordsSource = cacheRecordsGenerator.Generate();
-                Console.WriteLine(cacheRecordsSource);
+                GenerateCacheRecords(schema);
                 break;
+
             default:
                 Console.WriteLine($"Unknown command '{args[1]}'");
                 break;
         }
 
         Console.ReadKey();
+    }
+
+    private static void GenerateCacheRecords(Schema schema)
+    {
+        var cacheRecordsGenerator = new ParserGenerator(schema, "PivotCacheRecordsReader", "_ns")
+                .WithNamespace("ClosedXML.Excel.IO")
+                .AddSimpleTypeRequired<uint>("xsd:unsignedInt", "_reader.GetUInt(\"{0}\")")
+                .AddSimpleTypeOptional<int?>("xsd:int", "_reader.GetOptionalInt(\"{0}\")")
+                .AddSimpleTypeRequired<bool>("xsd:boolean", "_reader.GetBool(\"{0}\")")
+                .AddSimpleTypeOptional<bool?>("xsd:boolean", "_reader.GetOptionalBool(\"{0}\")")
+                .AddSimpleTypeOptional<string?>("s:ST_Xstring", "_reader.GetOptionalXString(\"{0}\")")
+                .AddSimpleTypeRequired<string>("s:ST_Xstring", "_reader.GetXString(\"{0}\")")
+                .AddSimpleTypeOptional<uint?>("xsd:unsignedInt", "_reader.GetOptionalUInt(\"{0}\")")
+                .AddSimpleTypeRequired<DateTime>("xsd:dateTime", "_reader.GetDateTime(\"{0}\")")
+                .AddSimpleTypeOptional<uint?>("ST_UnsignedIntHex", "_reader.GetOptionalUIntHex(\"{0}\")")
+                .AddSimpleTypeRequired<double>("xsd:double", "_reader.GetDouble(\"{0}\")")
+
+                // CT_PivotCacheRecords - hand-coded
+                .AddParseMethod("CT_Record")
+                .AddParseMethod("CT_Missing")
+                .AddParseMethod("CT_Number")
+                .AddParseMethod("CT_Boolean")
+                .AddParseMethod("CT_Error")
+                .AddParseMethod("CT_String")
+                .AddParseMethod("CT_DateTime")
+                .AddParseMethod("CT_Index")
+                .AddParseMethod("CT_X")
+                .AddParseMethod("CT_Tuples")
+                .AddParseMethod("CT_Tuple")
+            ;
+
+        var cacheRecordsSource = cacheRecordsGenerator.Generate();
+        Console.WriteLine(cacheRecordsSource);
     }
 }
